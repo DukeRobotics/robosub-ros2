37b9ae26d6220314f0981d08f3b9afb8edd053fa
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-09-07 17:56:07 -0400

    Rename core2 to core, onboard2 to onboard

--- docker/ros_bashrc.sh
+++ docker/ros_bashrc.sh
@@ -6,7 +6,7 @@ export PYTHONWARNINGS="ignore:easy_install command is deprecated"
 # To be appended to /root/.bashrc
 source /opt/ros/jazzy/setup.bash
 source /usr/share/colcon_cd/function/colcon_cd.sh
-source /root/dev/robosub-ros2/core2/install/setup.bash
-source /root/dev/robosub-ros2/onboard2/install/setup.bash
+source /root/dev/robosub-ros2/core/install/setup.bash
+source /root/dev/robosub-ros2/onboard/install/setup.bash
 
 export _colcon_cd_root=/opt/ros/jazzy
