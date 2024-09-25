c473f9dd53fcf48572e5af0e1e2e869fd78ec29c
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-09-25 23:59:40 +0000

    Added core to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 original_cwd=$(pwd)
+cd /root/dev/robosub-ros2/core
+colcon build --symlink-install --executor sequential
 cd /root/dev/robosub-ros2/onboard
 colcon build --symlink-install --executor sequential
 source install/setup.bash
