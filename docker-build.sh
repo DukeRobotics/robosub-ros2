c9431d7b0390fd67440c19e17ed638f6c6408145
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-09-28 19:01:52 -0400

    Compatability fixes for Max :)

--- docker-build.sh
+++ docker-build.sh
@@ -9,11 +9,13 @@ set -o allexport
 GIT_USER_NAME=$(git config --global user.name)
 GIT_USER_EMAIL=$(git config --global user.email)
 
-# Read SSH key contents using the paths from the .env file
-GITHUB_AUTH_SSH_KEY=$(cat ${GITHUB_AUTH_SSH_KEY_PRIV_PATH})
-GITHUB_AUTH_SSH_KEY_PUB=$(cat ${GITHUB_AUTH_SSH_KEY_PUB_PATH})
-GITHUB_SIGNING_SSH_KEY=$(cat ${GITHUB_SIGNING_SSH_KEY_PRIV_PATH})
-GIT_ALLOWED_SIGNERS=$(cat ${GIT_ALLOWED_SIGNERS_PATH})
+if [ "$NO_GIT" != "true" ]; then
+    # Read SSH key contents using the paths from the .env file
+    GITHUB_AUTH_SSH_KEY=$(cat ${GITHUB_AUTH_SSH_KEY_PRIV_PATH})
+    GITHUB_AUTH_SSH_KEY_PUB=$(cat ${GITHUB_AUTH_SSH_KEY_PUB_PATH})
+    GITHUB_SIGNING_SSH_KEY=$(cat ${GITHUB_SIGNING_SSH_KEY_PRIV_PATH})
+    GIT_ALLOWED_SIGNERS=$(cat ${GIT_ALLOWED_SIGNERS_PATH})
+fi
 
 # Build the Docker image with the SSH keys passed as build arguments
 docker compose up -d --build
