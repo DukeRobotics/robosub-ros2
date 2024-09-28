7194624c3fe6b60044800bc2fea3282cc5e8e678
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-09-28 14:48:09 -0400

    Added startup.sh and removed readonly from docker compose SSH mounts

---
+++ docker/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/github_auth_key
+chmod 644 ~/.ssh/github_auth_key.pub
+chmod 600 ~/.ssh/github_signing_key
+chmod 600 ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/allowed_signers
