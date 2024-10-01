fe84eeac298e2ddc564c242648358fb8a9e319f5
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-10-01 06:02:01 +0000

    Added checks in ros_bashrc to source setup files only if they exist

--- docker/ros_bashrc.sh
+++ docker/ros_bashrc.sh
@@ -6,7 +6,19 @@ export PYTHONWARNINGS="ignore:easy_install command is deprecated"
 # To be appended to /root/.bashrc
 source /opt/ros/jazzy/setup.bash
 source /usr/share/colcon_cd/function/colcon_cd.sh
-source /root/dev/robosub-ros2/core/install/setup.bash
-source /root/dev/robosub-ros2/onboard/install/setup.bash
+
+# Check if the core setup file exists and source it
+if [ -f /root/dev/robosub-ros2/core/install/setup.bash ]; then
+    source /root/dev/robosub-ros2/core/install/setup.bash
+else
+    echo "Core setup.bash not found, skipping."
+fi
+
+# Check if the onboard setup file exists and source it
+if [ -f /root/dev/robosub-ros2/onboard/install/setup.bash ]; then
+    source /root/dev/robosub-ros2/onboard/install/setup.bash
+else
+    echo "Onboard setup.bash not found, skipping."
+fi
 
 export _colcon_cd_root=/opt/ros/jazzy
