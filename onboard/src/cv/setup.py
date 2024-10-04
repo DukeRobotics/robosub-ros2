ff0feaeec850bdf22bdaadb6d8bec9295f55dc67
Mathew Chu <mathew.chu@duke.edu>
2024-10-03 20:16:44 -0400

    adding package deps v2
    updated setup.py

--- onboard/src/cv/setup.py
+++ onboard/src/cv/setup.py
@@ -24,11 +24,22 @@ setup(
     zip_safe=True,
     maintainer='root',
     maintainer_email='root@todo.todo',
-    description='TODO: Package description',
+    description='Computer Vision pipeline, including depthai and hsv filtering.',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'bin_detector = scripts.bin_detector:main',
+            'blue_rectangle_detector = scripts.blue_rectangle_detector:main',
+            'buoy_detector_contour_matching = scripts.buoy_detector_contour_matching:main',
+            'depthai_camera_connect = scripts.depthai_camera_connect:main',
+            'depthai_mono_detection = scripts.depthai_mono_detection:main',
+            'depthai_publish_save_streams = scripts.depthai_publish_save_streams:main',
+            'depthai_spatial_detection = scripts.depthai_spatial_detection:main',
+            'path_marker_detector = scripts.path_marker_detector:main',
+            'pink_bins_detector = scripts.pink_bins_detector:main',
+            'usb_camera_connect = scripts.usb_camera_connect:main',
+            'usb_camera = scripts.usb_camera:main',
         ],
     },
 )
