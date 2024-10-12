2d6feb3e587f37c693e98743b4f15f8da7955110
Lab Computer <lab-computer@duke-robotics.com>
2024-10-12 00:30:09 +0000

    Added skip-wsl to docker build

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the first argument is "skip-wsl" and if the script is running in WSL
+if [[ "$1" == "skip-wsl" && -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+    echo "'skip-wsl' is specified and the script is running in WSL. Exiting..."
+    exit 0
+fi
+
 # Load variables from .env file
 set -o allexport
 source .env
