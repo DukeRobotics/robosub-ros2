0beb15707f952b79c28064b4b23ef5dd1fe488ec
Will Denton <Willdenton@outlook.com>
2024-10-24 19:38:02 -0400

    small changes and launch file

--- onboard/src/data_pub/setup.py
+++ onboard/src/data_pub/setup.py
@@ -12,6 +12,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config', glob('./config/*.yaml')),
+        ('share/' + package_name + '/launch', glob('./launch/*_launch.xml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
