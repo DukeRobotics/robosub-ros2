d5daddac1b9aa90f2f13b938e324ce6f71c32901
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-10-26 15:31:32 +0000

    Migrate jsonpickle_custom_handlers

--- onboard/src/task_planning/task_planning/message_conversion/jsonpickle_custom_handlers.py
+++ onboard/src/task_planning/task_planning/message_conversion/jsonpickle_custom_handlers.py
@@ -1,11 +1,16 @@
 import builtins
 import jsonpickle
 import traceback
+import rosidl_runtime_py
+from rclpy.logging import get_logger
+from rosidl_runtime_py import get_message_interfaces
 
 
 from task_planning.message_conversion.ros_message_converter import convert_ros_message_to_dictionary, \
     convert_dictionary_to_ros_message
 
+
+logger = get_logger('jsonpickle_custom_handlers')
 
 class ROSMessageHandler(jsonpickle.handlers.BaseHandler):
     """
@@ -47,9 +52,22 @@ def register_custom_jsonpickle_handlers():
     """
     Register all custom JSONPickle handlers
     """
-
-    # TODO:ros2
-    # genpy no longer used in ROS2. Doesn't appear to be a base class for all ROS2 messages anymore...
-    # jsonpickle.handlers.register(genpy.Message, ROSMessageHandler, base=True)
+    # Register ROS 2 message types (including custom messages)
+    message_types = get_all_message_types()
+    for message_type in message_types:
+        jsonpickle.handlers.register(message_type, ROSMessageHandler, base=True)
+    logger.info(f'Registered {len(message_types)} message types')
 
     jsonpickle.handlers.register(BaseException, BaseExceptionHandler, base=True)
+
+def get_all_message_types():
+    message_interfaces = rosidl_runtime_py.get_message_interfaces()
+
+    all_message_types = []
+    for package_name, message_names in message_interfaces.items():
+        for message_name in message_names:
+            module = __import__(f'{package_name}.msg', fromlist=[message_name])
+            message_type = getattr(module, message_name.split('/')[-1])
+            all_message_types.append(message_type)
+
+    return all_message_types
