f226abb5d896ce1d9ffab130a41f88c3c543ecf2
Will Denton <Willdenton@outlook.com>
2024-10-26 14:28:41 -0400

    😈

--- onboard/src/static_transforms/launch/static_transforms.launch.py
+++ onboard/src/static_transforms/launch/static_transforms.launch.py
@@ -1,14 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
+# DEFINE TRANSFORMS HERE
 CORNER_LINK_TRANSFORM = [0.219075, -0.3095625, 0.19764375, 0, 0, 0, 'base_link', 'corner_link']
 DVL_LINK_TRANSFORM = [-0.20955, 0.0809625, -0.276225, 0, 0, 0, 'corner_link', 'dvl_link']
 IMU_LINK_TRANSFORM = [-0.219075, 0.3095625, -0.11241875, 0, 0, 0, 'corner_link', 'imu_link']
 SONAR_LINK_TRANSFORM = [0.067, 0.31, 0.05, 0, 0, 0, 'corner_link', 'sonar_link']
 CAMERAS_LINK_TRANSFORM = [0, 0.0362, 0, 0, 0, 0, 'corner_link', 'cameras_link']
 
-
+# Create the tf2 node for each transform
 def make_transform_publisher(transform):
     return Node(package='tf2_ros',
                 executable='static_transform_publisher',
@@ -22,7 +22,7 @@ def make_transform_publisher(transform):
                            '--frame-id', transform[6],
                            '--child-frame-id', transform[7]])
 
-
+# Create the launch description and populate
 def generate_launch_description():
     ld = LaunchDescription()
 
