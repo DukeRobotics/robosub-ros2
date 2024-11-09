634fd7feeae883828730f73c407630c878fa14a1
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-11-09 13:19:18 +0000

    Fix typo

--- onboard/src/static_transforms/setup.py
+++ onboard/src/static_transforms/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Duke Robotics',
-    maintainer_email='hello@dukerobotics.com',
+    maintainer_email='hello@duke-robotics.com',
     description='TODO: Package description',
     license='MIT',
 )
