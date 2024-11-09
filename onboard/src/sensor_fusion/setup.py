8f91257e1898f812836f9a78eeb4e6c6c6bd8847
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-11-09 07:55:10 -0500

    Update emails

--- onboard/src/sensor_fusion/setup.py
+++ onboard/src/sensor_fusion/setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
-    maintainer_email='oogway@duke-robotics.com',
+    maintainer_email='hello@duke-robotics.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
