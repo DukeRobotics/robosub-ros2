6fdb140c51450d06c36fdd20058ac1a085cc4fbb
Mathew Chu <mathew.chu@duke.edu>
2024-11-14 20:56:57 -0500

    Updates to usb_camera scrips

--- onboard/src/cv/cv/usb_camera_connect.py
+++ onboard/src/cv/cv/usb_camera_connect.py
@@ -5,17 +5,15 @@ from rclpy.node import Node
 from rclpy.parameter import Parameter
 import yaml
 import os
-from launch import LaunchDescription
+from launch import LaunchDescription, LaunchService
 from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription
 from launch_ros.actions import Node as LaunchNode
 import launch_ros
 
-CAMERA_CONFIG_PATH = 'package://cv/configs/usb_cameras.yaml'
-
 def connect_all(node):
     # get camera specs
     # In ROS2, we need to load the YAML file directly.
-    camera_config_path = os.path.expandvars(CAMERA_CONFIG_PATH.replace('package://', ''))
+    camera_config_path = './onboard/src/cv/config/usb_cameras.yaml'
     with open(camera_config_path, 'r') as f:
         cameras = yaml.safe_load(f)
 
@@ -25,12 +23,11 @@ def connect_all(node):
     for camera_name, camera in cameras.items():
         device_path = camera["device_path"]
         topic = camera["topic"]
-
         # Declare node and arguments for each camera
         launch_nodes.append(
             LaunchNode(
                 package='cv',
-                executable='usb_camera_node',
+                executable='usb_camera',
                 name=camera_name,
                 output='screen',
                 parameters=[{'topic': topic, 'device_path': device_path, 'framerate': 10}],
@@ -51,11 +48,8 @@ def generate_launch_description():
 def main():
     rclpy.init()
 
-    # Initialize a ROS2 Node (though not used directly here, it's good practice to initialize it)
-    node = Node('usb_camera_connect')
-
     # Use a launch file in ROS2
-    launch_service = launch_ros.actions.LaunchService()
+    launch_service = LaunchService()
     launch_service.include_launch_description(generate_launch_description())
 
     launch_service.run()
