59c1c57c6446fb4b128a0d1c2992eeb3e52795a2
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-11-23 20:22:45 +0000

    Added comments to github actions build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # shellcheck disable=SC1091
+# This script is used by GitHub Actions to build all ROS 2 packages
 
+# Exit immediately if a command exits with a non-zero status
+# This ensures the exit code of the script is 1 if any command fails, and 0 if all commands succeed
 set -e
 
 source /root/ros_bashrc.sh
