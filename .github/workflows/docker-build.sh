6895cbdd2156c3693803fb798409fe24827973ef
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-11-23 03:57:45 +0000

    Removed unnecessary docker-build.sh in github workflows

--- .github/workflows/docker-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2035
-
-docker buildx bake --file docker-compose.yml --set *.cache-from=type=local,src=/tmp/.buildx-cache --set *.cache-to=type=local,dest=/tmp/.buildx-cache
-docker compose -f docker-compose.yml up -d
