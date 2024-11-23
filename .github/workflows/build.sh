adb716ee79a67bd30b48a495073be6ce227a4acf
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-11-23 20:16:05 +0000

    Removed unnecessary code in github actions build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,30 +7,3 @@ source /root/ros_bashrc.sh
 
 source /root/dev/robosub-ros2/build.sh clean
 source /root/dev/robosub-ros2/build.sh
-exit 0
-
-# Directories for the core and onboard workspaces
-CORE_WS="/root/dev/robosub-ros2/core"
-ONBOARD_WS="/root/dev/robosub-ros2/onboard"
-
-# Delete the build, install, and log directories in the workspace
-clean_workspace() {
-    workspace_dir=$1
-    echo "Cleaning workspace: $workspace_dir"
-    rm -rf "$workspace_dir/build" "$workspace_dir/install" "$workspace_dir/log"
-}
-
-# Build the workspace
-build_workspace() {
-    workspace_dir=$1
-    echo "Building workspace: $workspace_dir"
-    cd "$workspace_dir"
-    colcon build --symlink-install
-    source install/setup.bash
-}
-
-clean_workspace "$CORE_WS"
-clean_workspace "$ONBOARD_WS"
-
-build_workspace "$CORE_WS"
-build_workspace "$ONBOARD_WS"
