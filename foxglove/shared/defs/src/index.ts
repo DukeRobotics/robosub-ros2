1cabe7b10057262e8a903e47284039f4241b8f7a
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-11-24 16:55:41 +0000

    Use generateRosTsGeneratorConfig in index

--- foxglove/shared/defs/src/index.ts
+++ foxglove/shared/defs/src/index.ts
@@ -1,6 +1,7 @@
 import { ros1 } from "@foxglove/rosmsg-msgs-common";
 
 import { writeAllDatatypeMaps } from "./datatypeMaps";
+import { generateRosTsGeneratorConfig } from "./generateRosTsGeneratorConfig";
 import { writeMessageDefinitions } from "./messageDefinitions";
 
 // The following paths are relative to the src directory containing this script
@@ -12,6 +13,13 @@ const DATATYPE_MAPS_SAVE_DIR = "datatype_maps/dist";
 const MSG_DEFS_RELATIVE_PATH = "../../custom_msg_defs/dist";
 
 async function main() {
+  generateRosTsGeneratorConfig(
+    "/opt/ros/jazzy/share/",
+    "ros_ts_generator_configs/dist/ros-ts-generator-config.json",
+    "../../types/dist/types.ts",
+    [{ namespace: "custom_msgs", path: "../../../../../core/src/custom_msgs" }],
+  );
+
   // Get message definitions for custom msgs and export them to files
   const definitionsByGroup = await writeMessageDefinitions(CUSTOM_MSGS_DEFS_PATH, CUSTOM_MSGS_SAVE_DIR);
 
