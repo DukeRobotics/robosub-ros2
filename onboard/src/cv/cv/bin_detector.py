10f4b97c3567c50e8212374693c48e101d552e3b
Oogway <oogway@duke-robotics.com>
2024-12-07 15:03:52 -0500

    Changes from pool test

--- onboard/src/cv/cv/bin_detector.py
+++ onboard/src/cv/cv/bin_detector.py
@@ -42,10 +42,10 @@ class BinDetector(Node):
         self.bin_center_contour_image_pub = self.create_publisher(Image, "/cv/bottom/bin_center/contour_image", 10)
         self.bin_center_bounding_box_pub = self.create_publisher(CVObject, "/cv/bottom/bin_center/bounding_box", 10)
         self.bin_center_distance_pub = self.create_publisher(Point, "/cv/bottom/bin_center/distance", 10)
-        print("Init")
+        ##print("Init")
 
     def image_callback(self, data):
-        print("Image Callback")
+        ##print("Image Callback")
         # Convert the compressed ROS image to OpenCV format
         np_arr = np.frombuffer(data.data, np.uint8)
         frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
@@ -54,7 +54,7 @@ class BinDetector(Node):
         self.process_frame(frame)
 
     def process_frame(self, frame):
-        print("process frame 1")
+        ##print("process frame 1")
         # Convert frame to HSV color space
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -101,15 +101,15 @@ class BinDetector(Node):
             # only processes if area (in pixels) if selected contour >500
             # publishes bbox, image, distance
             bbox, image, dist = self.process_contours(frame.copy(), contours_blue)
-            print("process frame 2")
+            #print("process frame 2")
             if bbox and image and dist and cv2.contourArea(contours_blue) > 500:
                 self.blue_bin_contour_image_pub.publish(image)
                 self.blue_bin_bounding_box_pub.publish(bbox)
                 self.blue_bin_distance_pub.publish(dist)
-                print("process frame 3")
+                #print("process frame 3")
 
         if contours_red:
-            print("process frame 4")
+            #print("process frame 4")
             # takes largest contour
             contours_red = sorted(contours_red, key=cv2.contourArea, reverse=True)
             contours_red = contours_red[0]
@@ -118,7 +118,7 @@ class BinDetector(Node):
             # publishes bbox, image, distance
             bbox, image, dist = self.process_contours(frame.copy(), contours_red)
             if bbox and image and dist and cv2.contourArea(contours_red) > 500:
-                print("process frame 5")
+                #print("process frame 5")
                 self.red_bin_contour_image_pub.publish(image)
                 self.red_bin_bounding_box_pub.publish(bbox)
                 self.red_bin_distance_pub.publish(dist)
@@ -202,11 +202,11 @@ class BinDetector(Node):
 def main(args=None):
     rclpy.init(args=args)
     bin_detector = BinDetector()
-    print("Node created")
+    #print("Node created")
     try:
-        print("Trying to spin node")
+        #print("Trying to spin node")
         rclpy.spin(bin_detector)
-        print("Node spun")
+        #print("Node spun")
     except KeyboardInterrupt:
         pass
     finally:
