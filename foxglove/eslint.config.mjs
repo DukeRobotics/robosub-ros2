db9ce9627dad4198bfa49ba18640f5309b9c9ae4
Maxwell Lin <git@maxwellmlin.com>
2024-12-26 01:26:58 +0000

    Migrate stylistic rules

--- foxglove/eslint.config.mjs
+++ foxglove/eslint.config.mjs
@@ -9,11 +9,37 @@ const __dirname = path.dirname(__filename);
 const ignorePath = path.resolve(__dirname, ".eslintignore");
 
 export default config(includeIgnoreFile(ignorePath), {
-  extends: [foxglove.configs.base, foxglove.configs.typescript],
+  extends: [foxglove.configs.base, foxglove.configs.react, foxglove.configs.typescript],
   languageOptions: {
     parserOptions: {
       project: ["**/tsconfig.json"],
       tsconfigRootDir: __dirname,
     },
+  },
+  rules: {
+    "no-warning-comments": ["warn"],
+    camelcase: [
+      "warn",
+      {
+        properties: "always",
+      },
+    ],
+    "react-hooks/exhaustive-deps": ["error"],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "lf",
+      },
+    ],
+    "spaced-comment": [
+      "error",
+      "always",
+      {
+        block: {
+          balanced: true,
+        },
+      },
+    ],
+    "capitalized-comments": ["warn", "always", { ignoreConsecutiveComments: true }],
   },
 });
