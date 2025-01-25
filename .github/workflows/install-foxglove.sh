c2f0a46cc7e422dd51e6fdfe9ee2010ecf7294f5
Maxwell Lin <git@maxwellmlin.com>
2025-01-25 21:31:45 +0000

    Update docs

--- .github/workflows/install-foxglove.sh
+++ .github/workflows/install-foxglove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC1091
-# This script is used by GitHub Actions to install all Foxglove extensions
+# This script is used by GitHub Actions to install all custom Foxglove extensions
 
 # Exit immediately if a command exits with a non-zero status
 # This ensures the exit code of the script is 1 if any command fails, and 0 if all commands succeed
