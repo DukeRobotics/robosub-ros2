79bedd6b4cb1e1f041f0488fb1ece2efe13894c6
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-02-04 17:20:59 -0500

    Added compile arduino sketches step to github actions

---
+++ .github/workflows/compile-arduino-sketches.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+# This script is used by GitHub Actions to compile all Arduino sketches
+# This script must be run after the onboard workspace has been built
+
+# Exit immediately if a command exits with a non-zero status
+# This ensures the exit code of the script is 1 if any command fails, and 0 if all commands succeed
+set -e
+
+source /root/ros_bashrc.sh
+
+ros2 run offboard_comms arduino compile all
