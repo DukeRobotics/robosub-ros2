1463093fe24b05c40584909649f5e75e5199decc
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-02-12 01:36:20 +0000

    Updated execute setup.py with appropriate metadata

--- onboard/src/execute/setup.py
+++ onboard/src/execute/setup.py
@@ -12,17 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (str(Path('share') / package_name / 'launch'), list(map(str, Path('./launch').glob('*.launch.py')))),
+        (str(Path('share') / package_name / 'launch'), list(map(str, Path('launch').glob('*.xml')))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='oogway@duke-robotics.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+    maintainer='Duke Robotics',
+    maintainer_email='hello@duke-robotics.com',
+    description='This package contains launch files to start multiple nodes at once.',
+    license='MIT',
 )
