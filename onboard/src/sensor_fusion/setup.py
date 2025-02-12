ba726575e9c16e99ddf95fdd2a02754e8a4227da
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-02-12 01:35:49 +0000

    Set sensor fusion setup.py version to 0.0.0

--- onboard/src/sensor_fusion/setup.py
+++ onboard/src/sensor_fusion/setup.py
@@ -6,7 +6,7 @@ package_name = 'sensor_fusion'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
