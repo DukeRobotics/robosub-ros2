1bb4a976c6b462883012d51eeb786835454165a5
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-02-12 03:07:07 +0000

    Updated marker dropper interface to use SetServo type

--- onboard/src/task_planning/task_planning/interface/marker_dropper.py
+++ onboard/src/task_planning/task_planning/interface/marker_dropper.py
@@ -1,9 +1,9 @@
 # TODO: Update marker_dropper interface (see ROS 2 offboard_comms package)
 
+from custom_msgs.srv import SetServo
 from rclpy.logging import get_logger
 from rclpy.node import Node
 from rclpy.task import Future
-from std_srvs.srv import SetBool
 from task_planning.utils.other_utils import singleton
 
 logger = get_logger('marker_dropper_interface')
@@ -27,7 +27,7 @@ class MarkerDropper:
     def __init__(self, node: Node, bypass: bool = False) -> None:
         self.node = node
 
-        self.drop_marker_client = node.create_client(SetBool, self.SERVO_CONTROL_SERVICE)
+        self.drop_marker_client = node.create_client(SetServo, self.SERVO_CONTROL_SERVICE)
 
         if not bypass:
             while not self.drop_marker_client.wait_for_service(timeout_sec=1.0):
@@ -38,12 +38,13 @@ class MarkerDropper:
         Send a request to activate or deactivate the marker dropper servo.
 
         Args:
-            data (bool): True to drop the marker, False to stop the servo.
+            data (bool): True to drop the left marker, False to drop the right marker.
 
         Returns:
             Future: The result of the asynchronous service call.
         """
-        request = SetBool.Request()
-        request.data = data
+        request = SetServo.Request()
+        request.tag = 'M'
+        request.pwm = 1250 if data else 1750
 
         return self.drop_marker_client.call_async(request)
