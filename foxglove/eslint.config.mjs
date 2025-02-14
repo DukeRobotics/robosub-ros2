ddd48c88c0b95c9f3e83a2c38cc23adf93e3998b
Maxwell Lin <git@maxwellmlin.com>
2025-02-14 04:32:37 +0000

    Rename .eslintignore to eslintignore to fix deprecation warning

--- foxglove/eslint.config.mjs
+++ foxglove/eslint.config.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const ignorePath = path.resolve(__dirname, ".eslintignore");
+const ignorePath = path.resolve(__dirname, "eslintignore");
 
 export default config(includeIgnoreFile(ignorePath), {
   extends: [foxglove.configs.base, foxglove.configs.react, foxglove.configs.typescript],
