597f7fabad296fe382b90931027f58e4251108aa
Maxwell Lin <git@maxwellmlin.com>
2025-02-14 04:50:39 +0000

    Add foxglove linting to checks

---
+++ .github/workflows/lint-foxglove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+# This script is used by GitHub Actions to lint the Foxglove monorepo
+
+# Exit immediately if a command exits with a non-zero status
+# This ensures the exit code of the script is 1 if any command fails, and 0 if all commands succeed
+set -e
+
+source /root/ros_bashrc.sh
+
+fox lint
