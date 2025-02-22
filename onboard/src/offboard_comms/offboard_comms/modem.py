2b976fff359ecd8d09f0c8cd12af6e2f8a44ecaf
Patrick Zheng <pzheng111@gmail.com>
2025-02-22 20:55:29 +0000

    initial work on WaterLinked Modem-M16 library

---
+++ onboard/src/offboard_comms/offboard_comms/modem.py
@@ -0,0 +1,112 @@
+import os
+import time
+import struct
+
+from offboard_comms.serial_node import SerialNode
+
+
+class ModemPublisher(SerialNode):
+    """Serial publisher to publish and send data from WaterLinked M16 modems."""
+
+    SERIAL_DEVICE_NAME = 'wlmodem-m16'
+    CONFIG_FILE_PATH = f'package://offboard_comms/config/{os.getenv("ROBOT_NAME", "oogway")}.yaml'
+
+    BAUDRATE = 9600
+    NODE_NAME = 'wlmodem'
+    CONNECTION_RETRY_PERIOD = 1.0  # seconds
+    LOOP_RATE = 0.625  # Hz, loop every 1.6 seconds
+
+    DIAGNOSTIC_PACKET_SIZE = 18
+
+    modem_ready = True
+    change_channel_timer = time()
+
+    def __init__(self) -> None:
+        super().__init__(self.NODE_NAME, self.BAUDRATE, self.CONFIG_FILE_PATH, self.SERIAL_DEVICE_NAME, True,
+                         self.CONNECTION_RETRY_PERIOD, self.LOOP_RATE, use_nonblocking=True)
+
+    def get_ftdi_string(self) -> str:
+        """
+        Get the FTDI string for the WaterLinked Modem-M16.
+
+        Returns:
+            str: FTDI string for the WaterLinked Modem-M16.
+        """
+        return self._config['modem']['ftdi']
+
+    def process_line(self, line: str) -> None:
+        """
+        Process a line of serial data from the WaterLinked Modem-M16.
+
+        Args:
+            line (str): line to process
+        """
+        data = bytes(line)
+        if len(data) != self.DIAGNOSTIC_PACKET_SIZE or data[0] != ord('$') or data[-1] != ord('\n'):
+            return self.process_data
+        return self.process_diagnostic_report
+
+    def process_data(self, packet: bytes) -> str:
+        """
+        Process the message report received from the WaterLinked Modem-M16.
+
+        Args:
+            packet (bytes): raw data packet.
+
+        Returns:
+            str: the raw data in string format.
+        """
+        return str(packet)
+
+    def process_diagnostic_report(self, packet: bytes) -> dict[str, any]:
+        r"""
+        Process the diagnostic report received from the WaterLinked Modem-M16.
+
+        Args:
+            packet (bytes): raw packet (18 bytes) starting with '$' (0x24) and ending with '\n' (0x0A).
+
+        Returns:
+            dict[str, any]: A dictionary with decoded values.
+        """
+        # Get the 16 bytes after the '$' and before the '\n'
+        data = packet[1:17]
+        decoded = struct.unpack('<HBBBHBBBBBHBB', data)
+
+        return {
+            'TR_BLOCK': decoded[0],
+            'BER': decoded[1],
+            'SIGNAL_POWER': decoded[2],
+            'NOISE_POWER': decoded[3],
+            'PACKET_VALID': decoded[4],
+            'PACKET_INVALID': decoded[5],
+            'GIT_REV': decoded[6].to_bytes(1, 'little'),
+            'TIME': (decoded[9] << 16) | (decoded[8] << 8) | decoded[7],
+            'CHIP_ID': decoded[10],
+            'HW_REV': decoded[11] & 0b00000011,
+            'CHANNEL': (decoded[11] & 0b00111100) >> 2,
+            'TB_VALID': (decoded[11] & 0b01000000) >> 6,
+            'TX_COMPLETE': (decoded[11] & 0b10000000) >> 7,
+            'DIAGNOSTIC_MODE': decoded[12] & 0b00000001,
+            'LEVEL': (decoded[12] & 0b00001100) >> 2,
+        }
+
+    def change_channel(self, channel: int) -> None:
+        """
+        Change the channel the modem is on if the modem is ready for commands.
+
+        Args:
+            channel (int): New channel value for the modem to operate on.
+        """
+        if channel < 1 or channel > 12:
+            self.get_logger().error('Invalid channel, please input an int within [1, 12]')
+            return
+
+        byte_to_send = 
+        if self.modem_ready:
+            self.change_channel_timer = time()
+            self.writebytes(ord('c'))
+            while self.change_channel_timer < 1000:
+                continue
+            self.writebytes(ord('c'))
+            self.writebytes(ord())
+
