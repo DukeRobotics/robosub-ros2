a3511f27e42344b7eebb3af8a3b8692307dfe56d
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-02-25 00:07:54 -0500

    Removed task planning launch file

--- onboard/src/task_planning/setup.py
+++ onboard/src/task_planning/setup.py
@@ -12,7 +12,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', [str(p) for p in Path('./launch').glob('*')]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
