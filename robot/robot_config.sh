bba4a857782408c98db6503222e54976bf5adba3
Crush <crush@duke-robotics.com>
2025-03-01 03:35:56 +0000

    Fixed bug with dev container not starting on robot

--- robot/robot_config.sh
+++ robot/robot_config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # shellcheck disable=SC2139
 
-repo_root="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-alias onboard2="${repo_root}/docker-build.sh"
+repo_root="$(dirname $(dirname "$(realpath "${BASH_SOURCE[0]}")"))"
+alias onboard2="${repo_root}/robot/docker-build.sh"
 alias bashon2='docker exec -ti -w /home/ubuntu/robosub-ros2 onboard2 bash'
 alias dkill='[ "$(docker ps --format {{.Names}})" ] && docker kill $(docker ps --format {{.Names}}) || echo "No Docker containers are running."'
+export ROBOSUB_ROS2_COMPOSE_FILE_PATH="${repo_root}/robot/docker-compose.yml"
 unset repo_root
