c98091a82004a209d049b4f779b82824fe038454
Maxwell Lin <git@maxwellmlin.com>
2025-03-01 17:55:12 +0000

    lint

--- robot/robot_config.sh
+++ robot/robot_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2139
 
-repo_root="$(dirname $(dirname "$(realpath "${BASH_SOURCE[0]}")"))"
+repo_root="$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
 alias onboard2="${repo_root}/robot/docker-build.sh"
 alias bashon2='docker exec -ti -w /home/ubuntu/robosub-ros2 onboard2 bash'
 alias dkill='[ "$(docker ps --format {{.Names}})" ] && docker kill $(docker ps --format {{.Names}}) || echo "No Docker containers are running."'
