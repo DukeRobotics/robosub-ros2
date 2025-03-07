864d306720ecdaf920b90988c45cc55dc11daa57
vedarshshah <33162616+vedarshshah@users.noreply.github.com>
2025-03-06 20:45:34 -0500

    Pool Test March 1 (#63)
    * Fixed bug in robot_config
    
    * Fixed task runner bugs
    
    * Added depth_level to move to pos local
    
    * Final prequal code
    
    * Ensure transform is available even if transforms started publishing after min transform time and log messages every second
    
    * Created tasks for oogway_shell
    
    * Refactored robot name to tasks to use dict
    
    * Check received state instead of state
    
    * Organize imports
    
    * Added comment for yaw correction
    
    * None type checking for state.py
    
    ---------
    
    Co-authored-by: Oogway <oogway@duke-robotics.com>
    Co-authored-by: Hung Le <135019172+viethungle-vt1401@users.noreply.github.com>
    Co-authored-by: viethungle-vt1401 <brandon.viethungRI2001@gmail.com>

--- onboard/src/task_planning/task_planning/prequal_tasks.py
+++ onboard/src/task_planning/task_planning/prequal_tasks.py
@@ -29,24 +29,48 @@ async def prequal_task(self: Task) -> Task[None, None, None]:  # noqa: PLR0915
 
     DEPTH_LEVEL = State().depth
 
-    async def rotate_deg(angle: float, log_msg_prefix: str = 'Rotate') -> Coroutine[None, None, None]:
+    async def rotate_deg(angle: float, log_msg_prefix: str = 'Rotate', depth_level: float | None = None) -> \
+        Coroutine[None, None, None]:
         """
         Rotate the robot by the specified angle in degrees.
 
         Args:
             angle (float): The angle to rotate the robot in degrees. Positive angles rotate the robot to the left.
             log_msg_prefix (str): The prefix for the message logged after rotating. Defaults to 'Rotate'.
+            depth_level (float | None): The depth level to move the robot to. If this is not None, the robot will be
+                kept at that depth level. Defaults to None.
         """
         rad_angle = math.radians(angle)
         await move_tasks.move_to_pose_local(
             geometry_utils.create_pose(0, 0, 0, 0, 0, rad_angle),
+            depth_level=depth_level,
             parent=self)
         logger.info(f'{log_msg_prefix} {angle}')
+
+    async def move_with_directions(directions: list[tuple[float, float, float]], depth_level: float | None = None) -> \
+        Coroutine[None, None, None]:
+        """
+        Move the robot in the specified directions.
+
+        Args:
+            directions (list[tuple[float, float, float]]): The directions to move the robot in. Each tuple contains
+                the distance to move the robot in the X, Y, and Z directions respectively.
+            depth_level (float | None): The depth level to move the robot to. If this is not None, the Z value in the
+                directions will be ignored and the robot will be kept at that depth level. Defaults to None.
+        """
+        for direction in directions:
+            await move_tasks.move_to_pose_local(
+                geometry_utils.create_pose(direction[0], direction[1], direction[2], 0, 0, 0),
+                depth_level=depth_level,
+                parent=self)
+            logger.info(f'Moved to {direction}')
 
     async def track_lane_marker(distance: float, forward: bool, step_size: float = 1.0) -> \
         Coroutine[None, None, None]:
         """
         Move the robot along the X axis the specified distance centering the lane marker in the bottom mono cam's frame.
+
+        Keeps robot at DEPTH_LEVEL.
 
         Args:
             distance (float): The total distance to move the robot forwards or backwards in meters. Must be positive.
@@ -69,14 +93,6 @@ async def prequal_task(self: Task) -> Task[None, None, None]:  # noqa: PLR0915
         prev_touching_bottom = False
 
         for i in range(repeats):
-            step = distance - total_dist if i == repeats-1 else step_size
-            await move_tasks.move_to_pose_local(
-                geometry_utils.create_pose(step * direction_sign, 0, 0, 0, 0, 0),
-                parent=self)
-
-            total_dist += step
-            logger.info(f'Moved {direction_term} {total_dist}')
-
             # If the lane marker is partially cut off in the bottom mono camera's frame,
             # then one or both of these flags will be true
 
@@ -96,26 +112,43 @@ async def prequal_task(self: Task) -> Task[None, None, None]:  # noqa: PLR0915
             if touching_top and not touching_bottom:
                 await move_tasks.move_to_pose_local(
                     geometry_utils.create_pose(0, 0.2, 0, 0, 0, 0),
+                    depth_level=DEPTH_LEVEL,
                     parent=self)
                 logger.info('Touching top correction: Moved 0.2 to the left')
 
                 # If the lane marker touched the top of the frame in the previous step as well despite the yaw
-                # correction, then it means the robot's yaw has drifted significantly to the right, so rotate left
+                # correction, then it means the robot's yaw has drifted significantly.
+                # If the robot is moving forward, this means the yaw has drifted to the right, so rotate left.
+                # If the robot is moving backward, this means the yaw has drifted to the left, so rotate right.
                 if prev_touching_top and not prev_touching_bottom:
-                    await rotate_deg(20)
+                    await rotate_deg(20 * direction_sign, depth_level=DEPTH_LEVEL)
 
             # If the lane marker is touching the bottom but not the top,
             # then the robot is too far to the left, so move right
             elif not touching_top and touching_bottom:
                 await move_tasks.move_to_pose_local(
                     geometry_utils.create_pose(0, -0.2, 0, 0, 0, 0),
+                    depth_level=DEPTH_LEVEL,
                     parent=self)
                 logger.info('Touching bottom correction: Moved 0.2 to the right')
 
                 # If the lane marker touched the bottom of the frame in the previous step as well despite the yaw
-                # correction, then it means the robot's yaw has drifted significantly to the left, so rotate right
+                # correction, then it means the robot's yaw has drifted significantly.
+                # If the robot is moving forward, this means the yaw has drifted to the left, so rotate right.
+                # If the robot is moving backward, this means the yaw has drifted to the right, so rotate left.
                 if not prev_touching_top and prev_touching_bottom:
-                    await rotate_deg(-20)
+                    await rotate_deg(-20 * direction_sign, depth_level=DEPTH_LEVEL)
+
+            # Y correction so the robot is centered on the lane marker
+            dist_pixels = CV().cv_data['lane_marker_dist']
+            height_pixels = CV().cv_data['lane_marker_height']
+            dist_meters = dist_pixels * LANE_MARKER_HEIGHT_METERS / height_pixels
+            if abs(dist_meters) > 0:
+                await move_tasks.move_to_pose_local(
+                    geometry_utils.create_pose(0, dist_meters, 0, 0, 0, 0),
+                    depth_level=DEPTH_LEVEL,
+                    parent=self)
+                logger.info(f'Y correction {dist_meters}')
 
             # Yaw correction so the robot's X axis is parallel to the lane marker
             # The 'lane_marker_angle' provides the angle between the lane line and the robot's X axis. The coordinate
@@ -125,47 +158,33 @@ async def prequal_task(self: Task) -> Task[None, None, None]:  # noqa: PLR0915
             # must rotate in the opposite direction of the angle, hence the angle is negated.
             angle = (CV().cv_data['lane_marker_angle'] * -1)
             if abs(angle) > 0:
-                await rotate_deg(angle, 'Yaw correction')
-
-            # Y correction so the robot is centered on the lane marker
-            dist_pixels = CV().cv_data['lane_marker_dist']
-            height_pixels = CV().cv_data['lane_marker_height']
-            dist_meters = dist_pixels * LANE_MARKER_HEIGHT_METERS / height_pixels
-            if abs(dist_meters) > 0:
-                await move_tasks.move_to_pose_local(
-                    geometry_utils.create_pose(0, dist_meters, 0, 0, 0, 0),
-                    parent=self)
-                logger.info(f'Y correction {dist_meters}')
-
-            # Depth correction so the robot is at the correct depth
-            depth_delta = DEPTH_LEVEL - State().depth
-            if abs(depth_delta) > 0:
-                await move_tasks.move_to_pose_local(
-                    geometry_utils.create_pose(0, 0, depth_delta, 0, 0, 0),
-                    parent=self)
-                logger.info(f'Depth correction {depth_delta}')
+                await rotate_deg(angle, 'Yaw correction', DEPTH_LEVEL)
 
             prev_touching_top = touching_top
             prev_touching_bottom = touching_bottom
 
+            # Move the robot forward or backward one step
+            step = distance - total_dist if i == repeats-1 else step_size
+            await move_tasks.move_to_pose_local(
+                geometry_utils.create_pose(step * direction_sign, 0, 0, 0, 0, 0),
+                depth_level=DEPTH_LEVEL,
+                parent=self)
+
+            total_dist += step
+            logger.info(f'Moved {direction_term} {total_dist}')
+
     # Move up to gate
-    await track_lane_marker(2.5, True)
+    await track_lane_marker(2, True)
 
     # Submerge below gate
-    await move_tasks.move_to_pose_local(
-        geometry_utils.create_pose(0, 0, -0.5, 0, 0, 0),
-        parent=self)
-    logger.info('Moved to (0, 0, -0.5)')
+    await move_with_directions([(0, 0, -0.5)])
     DEPTH_LEVEL = State().depth
 
     # Move through gate
-    await track_lane_marker(2, True)
+    await move_with_directions([(2.5, 0, 0)], depth_level=DEPTH_LEVEL)
 
     # Come back up
-    await move_tasks.move_to_pose_local(
-        geometry_utils.create_pose(0, 0, 0.2, 0, 0, 0),
-        parent=self)
-    logger.info('Moved to (0, 0, 0.2)')
+    await move_with_directions([(0, 0, 0.2)])
     DEPTH_LEVEL = State().depth
 
     # Move to buoy
@@ -173,40 +192,30 @@ async def prequal_task(self: Task) -> Task[None, None, None]:  # noqa: PLR0915
 
     # Dead reckon around buoy
     directions = [
-        (1, 0, 0),
-        (1, 0, 0),
-        (1, 0, 0),
-        (1, 0, 0),
-        (0, 1, 0),
-        (0, 1, 0),
-        (-1, 0, 0),
-        (-1, 0, 0),
-        (-1, 0, 0),
-        (0, -1, 0),
-        (0, -1, 0),
-        (0, -1, 0),
-        (0, -0.5, 0),
+        (3, 0, 0),
+        (0, 2, 0),
     ]
-    for direction in directions:
-        await move_tasks.move_to_pose_local(
-            geometry_utils.create_pose(direction[0], direction[1], direction[2], 0, 0, 0),
-            parent=self)
-        logger.info(f'Moved to {direction}')
+    await move_with_directions(directions, depth_level=DEPTH_LEVEL)
+
+    # Follow lane marker in adjacent lane backwards
+    await track_lane_marker(3, False)
+
+    # Move back to the original lane
+    await move_with_directions([(0, -2.5, 0)], depth_level=DEPTH_LEVEL)
 
     # Come back to gate
     await track_lane_marker(7, False)
 
-    # Robot is already deep enough to go through gate
+    # Move down to go through gate
+    await move_with_directions([(0, 0, -0.1)])
+    DEPTH_LEVEL = State().depth
 
     # Move back through gate
-    await track_lane_marker(2, False)
+    await move_with_directions([(-2.5, 0, 0)], depth_level=DEPTH_LEVEL)
 
     # Come back up
-    await move_tasks.move_to_pose_local(
-        geometry_utils.create_pose(0, 0, 0.2, 0, 0, 0),
-        parent=self)
-    logger.info('Moved to (0, 0, 0.2)')
+    await move_with_directions([(0, 0, 0.2)])
     DEPTH_LEVEL = State().depth
 
     # Return to start
-    await track_lane_marker(3, False)
+    await track_lane_marker(1, False)
