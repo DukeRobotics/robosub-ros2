7f5093d01b705e15c2c196add7a13adc5ac83767
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-03-22 00:47:52 +0000

    Added test task to oogway shell

--- onboard/src/task_planning/task_planning/robot/oogway_shell.py
+++ onboard/src/task_planning/task_planning/robot/oogway_shell.py
@@ -8,6 +8,8 @@ from task_planning.tasks import comp_tasks, move_tasks, prequal_tasks, test_task
 @task
 async def main(self: Task) -> Task[None, None, None]:
     """Run the tasks to be performed by Oogway Shell."""
-    tasks = []
+    tasks = [
+        test_tasks.print_task('Hello from Oogway Shell!', parent=self),
+    ]
     for task_to_run in tasks:
         await task_to_run
