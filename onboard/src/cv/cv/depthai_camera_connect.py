d9bbaa88698cba3c3cc0021b656b82de4b4de467
Oogway <oogway@duke-robotics.com>
2025-03-26 21:21:25 -0400

    Minor refactoring of depthai camera connect

--- onboard/src/cv/cv/depthai_camera_connect.py
+++ onboard/src/cv/cv/depthai_camera_connect.py
@@ -9,7 +9,7 @@ import resource_retriever as rr
 import yaml
 
 
-def connect(node: Node, camera_name: str, pipeline: dai.Pipeline) -> dai.Device:
+def connect(node: Node, camera: str, pipeline: dai.Pipeline) -> dai.Device:
     """
     Connect to the DepthAI camera and upload the pipeline.
 
@@ -18,7 +18,7 @@ def connect(node: Node, camera_name: str, pipeline: dai.Pipeline) -> dai.Device:
 
     Args:
         node (Node): The ROS2 node.
-        camera_name (str): The name of the camera to connect to.
+        camera (str): The name of the camera to connect to.
         pipeline (depthai.Pipeline): The DepthAI pipeline to upload to the camera.
 
     Returns:
@@ -32,9 +32,9 @@ def connect(node: Node, camera_name: str, pipeline: dai.Pipeline) -> dai.Device:
     with Path(rr.get_filename(config_file_path, use_protocol=False)).open() as f:
         config = yaml.safe_load(f)
 
-    # Check if the camera name is valid
-    if camera_name not in config["depthai"]["devices"]:
-        error_msg = f'Invalid camera name: "{camera_name}". Valid names are: {list(config["depthai"]["devices"].keys())}'
+    # Check if the camera is valid
+    if camera not in config["depthai"]["devices"]:
+        error_msg = f'Invalid camera: "{camera}". Valid cameras are: {list(config["depthai"]["devices"].keys())}'
         raise ValueError(error_msg)
 
     # Number of attempts that will be made to connect to the camera
@@ -44,30 +44,32 @@ def connect(node: Node, camera_name: str, pipeline: dai.Pipeline) -> dai.Device:
         if not rclpy.ok():
             break
 
-        node.get_logger().info(f'Attempting to connect to DepthAI camera {camera_name}, try {i + 1}...')
+        node.get_logger().info(f'Attempting to connect to DepthAI camera {camera}, try {i + 1}...')
 
         try:
             # Scan for camera IP address using custom autodiscovery
             ip = custom_autodiscovery(
-                config["depthai"]["devices"][camera_name]["mac"],
+                config["depthai"]["devices"][camera]["mac"],
                 config["depthai"]["ip_range"]
             )
 
             # Try connecting with the discovered IP address
             # If the execution reaches the following return statement, the lines above did not raise an exception, so a
             # successful camera connection was made, and device should be returned
-            return dai.Device(pipeline, dai.DeviceInfo(ip))
-        
+            device = dai.Device(pipeline, dai.DeviceInfo(ip))
+            node.get_logger().info(f'Connected to DepthAI camera {camera} at IP {ip}')
+            return device
+
         except RuntimeError:
             pass
 
         # Wait two seconds before trying again
         # This ensures the script does not terminate if the camera is just temporarily unavailable
         rclpy.spin_once(node, timeout_sec=2)
-    
+
     error_message = f'{total_tries} attempts were made to connect to the DepthAI camera using autodiscovery and static \
           IP address specification. All attempts failed.'
-    
+
     raise RuntimeError(error_message)
 
 
@@ -101,14 +103,13 @@ def main(args: None = None) -> None:
     """Run the node."""
     rclpy.init(args=args)
     node = Node('depthai_camera_connect')
-    camera_name = node.declare_parameter('camera_name', 'front').get_parameter_value().string_value
+    camera = node.declare_parameter('camera', 'front').get_parameter_value().string_value
 
     try:
-        connect(node, camera_name, dai.Pipeline())
+        connect(node, camera, dai.Pipeline())
     except KeyboardInterrupt:
         pass
     finally:
-        node.get_logger().info('Connected to DepthAI camera successfully.')
         node.destroy_node()
         if rclpy.ok():
             rclpy.shutdown()
