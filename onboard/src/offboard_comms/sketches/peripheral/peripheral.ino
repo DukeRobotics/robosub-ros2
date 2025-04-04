0cd6082033b5ce4811b510f29b6a76f1c5a7a99b
Oogway <oogway@duke-robotics.com>
2025-04-04 01:45:04 +0000

    Fixed errors and comments in peripheral arduino

--- onboard/src/offboard_comms/sketches/peripheral/peripheral.ino
+++ onboard/src/offboard_comms/sketches/peripheral/peripheral.ino
@@ -34,6 +34,8 @@ void setupGyroTrigger() {
   // The ATmega328P has three hardware timers: Timer0, Timer1, and Timer2.
   // Timer0 is used for the Arduino's millis() and delay() functions, and Timer1 is used for the Servo library. Thus, Timer2 is available for use in this sketch.
   //
+  pinMode(11, OUTPUT); // OC2A
+  pinMode(3, OUTPUT);  // OC2B
 
   // TCCR2A and TCCR2A are the control registers for Timer2; they define how the timer behaves
   // Set the registers to 0 to clear any previous settings
@@ -44,20 +46,21 @@ void setupGyroTrigger() {
   TCNT2 = 0;
 
   // Setting the WGM21 bit means the timer is set to CTC (Clear Timer on Compare Match) mode
-  // This means that the timer will count up from 0 to the value in OCR2A, then reset to 0. OCR2B must be less than or equal to OCR2A.
-  // Setting the COM2A0 and COM2B0 bits means the outputs of pins OC2A and OC2B will be toggled when the timer reaches OCR2A and OCR2B respectively
+  // This means that the timer will count up from 0 to the value in OCR2A, then reset to 0
+  // Setting the COM2A0 and COM2B0 bits means the outputs of pins OC2A and OC2B will both be toggled when the timer reaches OCR2A
   TCCR2A = (1 << COM2A0) | (1 << COM2B0) | (1 << WGM21);
 
   // Setting the CS22 bit sets the prescale to 64, which means for every 64 clock cycles, the timer will increment by 1
-  // The FOC2B bit is set to immediately toggle the OC2B output when the timer is started, ensuring that its value is opposite to OC2A and thus creating a complementary output
+  // The FOC2B bit is set to immediately toggle the OC2B output when the timer is started
+  // This ensures that its starting value is opposite to OC2A and thus creating a complementary output
   TCCR2B = (1 << CS22) | (1 << FOC2B);
 
-  // The OCR2A and OCR2B registers define the values at which the timer will toggle the outputs
+  // The OCR2A register defines the values at which the timer will toggle the outputs
   // The ATmega328P has a CPU clock speed of 16MHz, and with a prescaler of 64, the timer increments 16,000,000 / 64 = 250,000 times per second
   // To create a 1000Hz square wave with 50% duty cycle, the timer must toggle the outputs at 2000Hz
-  // So set the OCR2A and OCR2B registers to 124, which means the timer will toggle the outputs 250,000 / 125 = 2000 times per second (the timer counts from 0 to 124, which is 125 counts)
+  // Set the OCR2A register to 124 so timer counts from 0 to 124, which is 125 counts
+  // Thus, the timer will toggle the outputs 250,000 / 125 = 2000 times per second
   OCR2A = 124;
-  OCR2B = 124;
 }
 
 void setup() {
