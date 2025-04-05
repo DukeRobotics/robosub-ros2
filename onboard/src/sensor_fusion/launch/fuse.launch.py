15c0f828f1e7eb34fd186194ed3adb56a1c4c424
Oogway <oogway@duke-robotics.com>
2025-04-05 06:29:51 +0000

    Added separate sensor fusion config for each robot

--- onboard/src/sensor_fusion/launch/fuse.launch.py
+++ onboard/src/sensor_fusion/launch/fuse.launch.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -14,13 +15,15 @@ def generate_launch_description() -> LaunchDescription:
     """
     ld = LaunchDescription()
 
+    robot_name = os.getenv('ROBOT_NAME')
+
     # Start robot localization using an Extended Kalman filter
     robot_localization = Node(
         package='robot_localization',
         executable='ekf_node',
         name='ekf_localization_node',
         output='screen',
-        parameters=[str(Path(get_package_share_directory('sensor_fusion')) / 'params' / 'main.yaml')],
+        parameters=[str(Path(get_package_share_directory('sensor_fusion')) / 'config' / f'{robot_name}.yaml')],
         remappings=[
             ('odometry/filtered', 'state'),
         ])
