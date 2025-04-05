b549a5da71818c78a5f80c8e1c76a99891ef6267
Crush <crush@duke-robotics.com>
2025-04-05 05:11:46 +0000

    Cleaned up DVL wayfinder code

--- onboard/src/offboard_comms/offboard_comms/dvl_wayfinder.py
+++ onboard/src/offboard_comms/offboard_comms/dvl_wayfinder.py
@@ -1,26 +1,25 @@
 import datetime as dt
 import math
-import time
-import traceback
-from pathlib import Path
 import os
-import yaml
+import time
+from pathlib import Path
 
 import numpy as np
 import rclpy
+import resource_retriever as rr
+import yaml
+from builtin_interfaces.msg import Time
+from geometry_msgs.msg import Twist, Vector3
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
 from serial.tools import list_ports
-import resource_retriever as rr
 
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
-from builtin_interfaces.msg import Time
-
-from offboard_comms.wayfinder_dvl.dvl import Dvl
-from offboard_comms.wayfinder_dvl.system import OutputData
+from offboard_comms.dvl_wayfinder_lib.dvl import Dvl
+from offboard_comms.dvl_wayfinder_lib.system import OutputData
 
 
-class Wayfinder_DVL(Node):
+class DVLWayfinderPublisher(Node):
+    """A class to read and publish Teledyne Wayfinder DVL data from a serial port."""
     CONFIG_FILE_PATH = f'package://offboard_comms/config/{os.getenv("ROBOT_NAME")}.yaml'
 
     WAYFINDER_ROLL = np.radians(180)
@@ -37,8 +36,11 @@ class Wayfinder_DVL(Node):
         with Path(rr.get_filename(self.CONFIG_FILE_PATH, use_protocol=False)).open() as f:
             self._config_data = yaml.safe_load(f)
 
+        ftdi = self._config_data['dvl']['ftdi']
+        self.get_logger().info(f'FTDI: {ftdi}')
+
         try:
-            self._serial_port = next(list_ports.grep('DN04A9W9')).device
+            self._serial_port = next(list_ports.grep(ftdi)).device
         except StopIteration:
             self.get_logger().error('Wayfinder DVL not found.')
             rclpy.shutdown()
@@ -46,15 +48,16 @@ class Wayfinder_DVL(Node):
         self._sensor = Dvl()
         self._sensor.connect(self._serial_port, 115200)
 
-        self.get_logger().info(f'Wayfinder connected at {self._serial_port}')
+        self.get_logger().info(f'Connected to DVL Wayfinder at {self._serial_port}.')
 
-        #set time of device
+        # Precisely set the time on the DVL
         self._sensor.enter_command_mode()
-        two_seconds = dt.datetime.now() + dt.timedelta(seconds=2)
-        time_target = dt.datetime(two_seconds.year, two_seconds.month, two_seconds.day, two_seconds.hour, two_seconds.minute, two_seconds.second)
-        now = dt.datetime.now()
+        two_seconds = dt.datetime.now(dt.UTC) + dt.timedelta(seconds=2)
+        time_target = dt.datetime(two_seconds.year, two_seconds.month, two_seconds.day, two_seconds.hour,
+                                  two_seconds.minute, two_seconds.second, tzinfo=dt.UTC)
+        now = dt.datetime.now(dt.UTC)
         time.sleep((time_target - now).total_seconds())
-        self._sensor.set_time(dt.datetime.now())
+        self._sensor.set_time(dt.datetime.now(dt.UTC))
         self._sensor.exit_command_mode()
 
         #setup rotation matrix
@@ -65,81 +68,78 @@ class Wayfinder_DVL(Node):
         self._pub = self.create_publisher(Odometry, self.DVL_ODOM_TOPIC, 50)
 
         #enable data callback
-        self._sensor.register_ondata_callback(self.wayfinderDataCallback, None)
+        self._sensor.register_ondata_callback(self.wayfinder_data_callback, None)
 
 
-    def wayfinderDataCallback(self, dataObj: OutputData, *args):
+    def wayfinder_data_callback(self, data_obj: OutputData) -> None:
         """
-        WayFinder Data Callback Function
+        Process Wayfinder data.
 
-        WayFinder Data Callback - this processed the data from the WayFinder and
-        passes it to the PixHawk autopilot
-
-        Arguments:
-            dataObj {OutputData} -- WayFinder output data
+        Args:
+            data_obj (OutputData): WayFinder output data
         """
-        try:
-            # Check velocities are valid
-            if math.isnan(dataObj.vel_x) or math.isnan(dataObj.vel_y) or math.isnan(dataObj.vel_z) or math.isnan(dataObj.vel_err):
-                return
+        # Check velocities are valid
+        if any(math.isnan(val) for val in [data_obj.vel_x, data_obj.vel_y, data_obj.vel_z, data_obj.vel_err]):
+            return
 
-            vels = np.array([dataObj.vel_x, dataObj.vel_y, dataObj.vel_z])
-            vels =  np.matmul(self._rotMatrix, vels)
+        vels = np.array([data_obj.vel_x, data_obj.vel_y, data_obj.vel_z])
+        vels =  np.matmul(self._rotMatrix, vels)
 
-            # Confidence calculation
-            vel_err_max = 1
-            vel_err_min = 0.001
-            pct_err_vel = 0.005
-            vel_mean = np.mean(vels)
-            confidence = 1 - (vel_mean - vel_err_min) / (vel_err_max - vel_err_min)
+        # Confidence calculation
+        vel_err_max = 1
+        vel_err_min = 0.001
+        pct_err_vel = 0.005
+        vel_mean = np.mean(vels)
+        confidence = 1 - (vel_mean - vel_err_min) / (vel_err_max - vel_err_min)
 
-            dvl_time = dt.datetime(dataObj.year, dataObj.month, dataObj.day, dataObj.hour, dataObj.minute, dataObj.second)
-            formatted_date = dvl_time.strftime("%Y-%m-%d %H:%M:%S")
+        dvl_time = dt.datetime(data_obj.year, data_obj.month, data_obj.day, data_obj.hour, data_obj.minute,
+                                data_obj.second, tzinfo=dt.UTC)
+        formatted_date = dvl_time.strftime('%Y-%m-%d %H:%M:%S')
 
-            self.get_logger().info('%9.3f %9.3f %9.3f | %9.3f | %9.3f %9.3f %9.3f %9.3f | %s %s' % (vels[0], vels[1], vels[2], dataObj.vel_err, dataObj.range_beam1, dataObj.range_beam2, dataObj.range_beam3, dataObj.range_beam4, dataObj.bit_code, formatted_date))
+        self.get_logger().info(f'{vels[0]:9.3f} {vels[1]:9.3f} {vels[2]:9.3f} | {data_obj.vel_err:9.3f} | '
+                                f'{data_obj.range_beam1:9.3f} {data_obj.range_beam2:9.3f} '
+                                f'{data_obj.range_beam3:9.3f} {data_obj.range_beam4:9.3f} | {data_obj.bit_code}'
+                                f'{formatted_date}')
 
-            # Convert to Unix timestamp (as float in seconds)
-            unix_timestamp = dvl_time.timestamp()
+        # Convert to Unix timestamp (as float in seconds)
+        unix_timestamp = dvl_time.timestamp()
 
-            # Convert to ROS2 Header stamp (sec and nanosec)
-            sec = int(unix_timestamp)
-            nanosec = int((unix_timestamp - sec) * 1e9)
+        # Convert to ROS2 Header stamp (sec and nanosec)
+        sec = int(unix_timestamp)
+        nanosec = int((unix_timestamp - sec) * 1e9)
 
-            # Create the ROS2 Time message
-            header_stamp = Time()
-            header_stamp.sec = sec
-            header_stamp.nanosec = nanosec
+        # Create the ROS2 Time message
+        header_stamp = Time()
+        header_stamp.sec = sec
+        header_stamp.nanosec = nanosec
 
-            # Publish the data
-            odom = Odometry()
-            odom.header.stamp = header_stamp
-            odom.header.frame_id = 'odom'
+        # Publish the data
+        odom = Odometry()
+        odom.header.stamp = header_stamp
+        odom.header.frame_id = 'odom'
 
-            # set pose
-            odom.child_frame_id = 'dvl'
+        # set pose
+        odom.child_frame_id = 'dvl'
 
-            # set twist (set angular velocity to (0, 0, 0), should not be used)
-            odom.twist.twist = Twist(linear=Vector3(x=dataObj.vel_x, y=dataObj.vel_y, z=dataObj.vel_z),
-                                     angular=Vector3(x=0.0, y=0.0, z=0.0))
-            odom.twist.covariance[0] = 0.01
-            odom.twist.covariance[7] = 0.01
-            odom.twist.covariance[14] = 0.01
-            self._pub.publish(odom)
-        except Exception as e:
-            self.get_logger().info(e)
-            traceback.print_exc()
+        # set twist (set angular velocity to (0, 0, 0), should not be used)
+        odom.twist.twist = Twist(linear=Vector3(x=data_obj.vel_x, y=data_obj.vel_y, z=data_obj.vel_z),
+                                    angular=Vector3(x=0.0, y=0.0, z=0.0))
+        odom.twist.covariance[0] = 0.01
+        odom.twist.covariance[7] = 0.01
+        odom.twist.covariance[14] = 0.01
+        self._pub.publish(odom)
 
 def main(args: list[str] | None = None) -> None:
     """Initialize and run the Sonar node."""
     rclpy.init(args=args)
-    wayfinder_dvl = Wayfinder_DVL()
+    dvl_wayfinder = DVLWayfinderPublisher()
 
     try:
-        rclpy.spin(wayfinder_dvl)
+        rclpy.spin(dvl_wayfinder)
     except KeyboardInterrupt:
         pass
     finally:
-        wayfinder_dvl.destroy_node()
+        dvl_wayfinder.destroy_node()
         if rclpy.ok():
             rclpy.shutdown()
 
