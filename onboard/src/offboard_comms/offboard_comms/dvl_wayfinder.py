83a46935338d2e47662aab7aeb41c4d39ca01bf9
Vedarsh Shah <vedarsh.shah@gmail.com>
2025-04-10 05:21:03 +0000

    More wayfinder dvl code cleanup

--- onboard/src/offboard_comms/offboard_comms/dvl_wayfinder.py
+++ onboard/src/offboard_comms/offboard_comms/dvl_wayfinder.py
@@ -24,7 +24,8 @@ class DVLWayfinderPublisher(Node):
     CONFIG_FILE_PATH = f'package://offboard_comms/config/{os.getenv("ROBOT_NAME")}.yaml'
     BAUDRATE = 115200
 
-    # DVL orientation in degrees
+    # Rotate the DVL's coordinate system by the following angles specified in degrees
+    # Extrinsic rotation is performed in the order of roll, pitch, yaw
     WAYFINDER_ROLL = 0
     WAYFINDER_PITCH = 0
     WAYFINDER_YAW = -135
@@ -36,6 +37,7 @@ class DVLWayfinderPublisher(Node):
     def __init__(self) -> None:
         super().__init__(self.NODE_NAME)
 
+        # Load the offboard comms config file
         with Path(rr.get_filename(self.CONFIG_FILE_PATH, use_protocol=False)).open() as f:
             self._config_data = yaml.safe_load(f)
 
@@ -53,17 +55,19 @@ class DVLWayfinderPublisher(Node):
         self.get_logger().info(f'Connected to DVL Wayfinder at {self._serial_port}.')
 
         # Precisely set the time on the DVL
+        # The DVL does not accept fractional seconds, so set the time precisely to the second by sleeping until the
+        # next to next second begins, then set the DVL time
         self._sensor.enter_command_mode()
         two_seconds = dt.datetime.now(dt.UTC) + dt.timedelta(seconds=2)
         time_target = dt.datetime(two_seconds.year, two_seconds.month, two_seconds.day, two_seconds.hour,
-                                  two_seconds.minute, two_seconds.second, tzinfo=dt.UTC)
+                                  two_seconds.minute, two_seconds.second, tzinfo=dt.UTC)  # Remove fractional seconds
         now = dt.datetime.now(dt.UTC)
         time.sleep((time_target - now).total_seconds())
         self._sensor.set_time(dt.datetime.now(dt.UTC))
         self._sensor.exit_command_mode()
 
         # Set up rotation matrix
-        self._rotMatrix = euler2mat(math.radians(self.WAYFINDER_ROLL), math.radians(self.WAYFINDER_PITCH),
+        self._rotation_matrix = euler2mat(math.radians(self.WAYFINDER_ROLL), math.radians(self.WAYFINDER_PITCH),
                                     math.radians(self.WAYFINDER_YAW))
 
         self._pub = self.create_publisher(Odometry, self.DVL_ODOM_TOPIC, 50)
@@ -103,24 +107,23 @@ class DVLWayfinderPublisher(Node):
         header_stamp.sec = int(unix_timestamp)
         header_stamp.nanosec = int((unix_timestamp - header_stamp.sec) * 1e9)
 
-
         # Apply rotation matrix to velocities
-        vels = np.matmul(self._rotMatrix, [data_obj.vel_x, data_obj.vel_y, data_obj.vel_z])
+        vels = np.matmul(self._rotation_matrix, [data_obj.vel_x, data_obj.vel_y, data_obj.vel_z])
 
         # Convert velocities to Vector3
-        vels = Vector3(x=vels[0], y=vels[1], z=vels[2])
+        vels_vector = Vector3(x=vels[0], y=vels[1], z=vels[2])
 
         # Negate velocities if configured
         if self._config_data['dvl']['negate_x_vel']:
-            vels.x = -vels.x
+            vels_vector.x = -vels_vector.x
         if self._config_data['dvl']['negate_y_vel']:
-            vels.y = -vels.y
+            vels_vector.y = -vels_vector.y
         if self._config_data['dvl']['negate_z_vel']:
-            vels.z = -vels.z
+            vels_vector.z = -vels_vector.z
 
         # Publish the data
         self.odom.header.stamp = header_stamp
-        self.odom.twist.twist.linear = vels
+        self.odom.twist.twist.linear = vels_vector
         self._pub.publish(self.odom)
 
 def main(args: list[str] | None = None) -> None:
