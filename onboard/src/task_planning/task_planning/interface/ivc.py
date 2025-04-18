17d5122aa7d570fda8ac01e2568956f125584c4b
Oogway <oogway@duke-robotics.com>
2025-04-18 22:51:45 +0000

    Refactored IVC interface as its own singleton

--- onboard/src/task_planning/task_planning/interface/ivc.py
+++ onboard/src/task_planning/task_planning/interface/ivc.py
@@ -1,34 +1,74 @@
+from dataclasses import dataclass
+from enum import Enum
+
 from custom_msgs.msg import ModemStatus, StringWithHeader
 from custom_msgs.srv import SendModemMessage
 from rclpy.logging import get_logger
 from rclpy.node import Node
+from rclpy.task import Future
+from rclpy.time import Time
 from task_planning.utils.other_utils import singleton
 
 logger = get_logger('ivc_interface')
 
-@singleton
+class IVCMessageType(Enum):
+    """Enum for the types of IVC messages."""
+
+    UNKNOWN = ''
+    OOGWAY_TEST = 'to'
+    CRUSH_TEST = 'tc'
+
+
+@dataclass
+class IVCMessage:
+    """
+    Dataclass to store IVC message properties.
+
+    Attributes:
+        timestamp (Time): The timestamp of the message.
+        msg (IVCMessageType): The type of the IVC message.
+    """
+    timestamp: Time
+    msg: IVCMessageType
+
+
 class IVC:
     """Interface for inter-vehicle communication (IVC)."""
+
+    _instance = None
 
     # ROS topics for the state and resetting the pose
     STATUS_TOPIC = '/sensors/modem/status'
     MESSAGES_TOPIC = '/sensors/modem/messages'
     SEND_MESSAGE_SERVICE = '/sensors/modem/send_message'
 
-    def __init__(self, node: Node, bypass: bool = False) -> None:
+    def __new__(cls, node: Node | None = None, bypass: bool = False) -> 'IVC':
+        if cls._instance is None:
+            if node is None:
+                raise ValueError("IVC must be initialized with a Node the first time.")
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
+    def __init__(self, node: Node | None = None, bypass: bool = False) -> None:
         """
         Initialize the state.
 
         Args:
-            node: The ROS node used for communication and state management.
-            bypass: If True, bypass certain checks. Defaults to False.
+            node (Node): The ROS node used for communication and state management.
+            bypass (bool): If True, bypass certain checks. Defaults to False.
         """
+        if self._initialized:
+            return
+
+        self._initialized = True
+
         self.bypass = bypass
 
         self._modem_status = ModemStatus()
         self._received_modem_status = False
 
-        self._messages = []
+        self._messages: list[IVCMessage] = []
 
         node.create_subscription(ModemStatus, self.STATUS_TOPIC, self._on_receive_modem_status, 10)
         node.create_subscription(StringWithHeader, self.MESSAGES_TOPIC, self._on_receive_modem_message, 10)
@@ -49,7 +89,7 @@ class IVC:
         return self._received_modem_status
 
     @property
-    def messages(self) -> list[StringWithHeader]:
+    def messages(self) -> list[IVCMessage]:
         """The list of messages received from the other robot."""
         return self._messages
 
@@ -58,28 +98,52 @@ class IVC:
         Store the received modem status message.
 
         Args:
-            msg: The received modem status message.
+            msg (ModemStatus): The received modem status message.
         """
         self._modem_status = msg
         self._received_modem_status = True
 
-    def _on_receive_modem_message(self, msg: StringWithHeader) -> None:
+    def _convert_stringwithheader_to_ivcmessage(self, msg: StringWithHeader) -> IVCMessage:
         """
-        Store the message received from the other robot.
+        Convert a StringWithHeader message to an IVCMessage.
+
+        If the StringWithHeader message contains data that is not a valid IVCMessageType, it will be set to UNKNOWN.
 
         Args:
-            msg: The message received from the other robot.
-        """
-        self._messages.append(msg)
+            msg (StringWithHeader): The StringWithHeader message to convert.
 
-    def send_message(self, msg: str) -> None:
+        Returns:
+            IVCMessage: The message converted to an IVCMessage.
+        """
+        timestamp = Time.from_msg(msg.header.stamp)
+        try:
+            message_type = IVCMessageType(msg.data)
+        except ValueError:
+            message_type = IVCMessageType.UNKNOWN
+        return IVCMessage(timestamp=timestamp, msg=message_type)
+
+    def _on_receive_modem_message(self, msg: StringWithHeader) -> None:
+        """
+        Convert the message received from the other robot to an IVCMessage and append it to the list of messages.
+
+        Args:
+            msg (StringWithHeader): The message received from the other robot.
+        """
+        self._messages.append(self._convert_stringwithheader_to_ivcmessage(msg))
+
+    def send_message(self, msg: IVCMessageType) -> Future | None:
         """
         Send a message to the other robot.
 
         Args:
-            msg: The message to send.
+            msg (IVCMessageType): The message to send.
+
+        Returns:
+            Future | None: The future of the service call if not bypassed, None otherwise.
         """
         request = SendModemMessage.Request()
-        request.message = msg
+        request.message = msg.value
         if not self.bypass:
-            self._send_message.call_async(request)
+            return self._send_message.call_async(request)
+
+        return None
