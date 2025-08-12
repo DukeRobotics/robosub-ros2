b9247538226b8ce521fba6dd8990ab888cfb1a2a
Oogway <oogway@duke-robotics.com>
2025-08-12 16:48:30 +0000

    Add enable_recording param to robot launch

--- onboard/src/execute/launch/robot.launch.py
+++ onboard/src/execute/launch/robot.launch.py
@@ -3,8 +3,9 @@ from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
@@ -29,6 +30,12 @@ def generate_launch_description() -> LaunchDescription:
     robot_name = os.getenv('ROBOT_NAME')
 
     ld = LaunchDescription()
+
+    ld.add_action(DeclareLaunchArgument(
+        'enable_recording',
+        default_value='false',
+        description='Enable or disable recording functionality'
+    ))
 
     ld.add_action(IncludeLaunchDescription(
         XMLLaunchDescriptionSource(str(pkg_controls / 'launch' / 'controls.xml')),
@@ -61,6 +68,9 @@ def generate_launch_description() -> LaunchDescription:
 
     ld.add_action(IncludeLaunchDescription(
         XMLLaunchDescriptionSource(str(pkg_system_utils / 'launch' / 'system_utils.xml')),
+        launch_arguments=[
+            ('enable_recording', LaunchConfiguration('enable_recording')),
+        ]
     ))
 
     ld.add_action(IncludeLaunchDescription(
