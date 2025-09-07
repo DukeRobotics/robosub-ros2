736eda6cd1c8f6dac569fb15d6c6657c8906d87f
Viet Hung Le <brandon.viethungRI2001@gmail.com>
2025-09-07 23:53:46 +0000

    Clean up oogway.py and crush.py task flow

--- onboard/src/task_planning/task_planning/robot/crush.py
+++ onboard/src/task_planning/task_planning/robot/crush.py
@@ -1,8 +1,9 @@
 # ruff: noqa: ERA001, F401
 from math import radians
+
 import numpy as np
-from task_planning.interface.ivc import IVCMessageType
 from task_planning.interface.cv import CVObjectType
+from task_planning.interface.ivc import IVCMessageType
 from task_planning.task import Task, task
 from task_planning.tasks import buoyancy_tasks, comp_tasks, ivc_tasks, move_tasks, prequal_tasks, sonar_tasks
 from task_planning.utils import geometry_utils
@@ -12,171 +13,60 @@ from task_planning.utils import geometry_utils
 async def main(self: Task) -> Task[None, None, None]:
     """Run the tasks to be performed by Crush."""
     tasks = [
-        # START 2025 COMP FLOW RETURN TO GATE
-        # comp_tasks.delineate_ivc_log(parent = self),
-        # comp_tasks.initial_submerge(-0.7, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # comp_tasks.coin_flip(enable_same_direction=False, parent=self),
-        # comp_tasks.gate_task_dead_reckoning(depth_level=-0.7, parent=self),
-        # comp_tasks.gate_style_task(depth_level=1.0, parent=self),
-        # comp_tasks.crush_ivc_send(msg_to_send = IVCMessageType.CRUSH_GATE,
-        #     msg_to_receive = IVCMessageType.OOGWAY_ACKNOWLEDGE, timeout=90, parent=self),
-        # comp_tasks.slalom_task_dead_reckoning(depth_level=1.1, parent=self),
-        # comp_tasks.crush_ivc_receive(msg_to_receive = IVCMessageType.OOGWAY_GATE,
-        #     msg_to_send = IVCMessageType.CRUSH_ACKNOWLEDGE, timeout=90, parent=self),
-        # comp_tasks.return_task_dead_reckoning(depth_level=1.1, parent=self),
-
-        # 2025 COMP HOME POOL TEST
-        # comp_tasks.delineate_ivc_log(parent = self),
-        # comp_tasks.initial_submerge(-0.7, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # # comp_tasks.coin_flip(enable_same_direction=False, parent=self),
-        # # move_tasks.move_with_directions([(1, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # comp_tasks.gate_style_task(depth_level=1.1, parent=self),
-        # comp_tasks.crush_ivc_send(msg_to_send = IVCMessageType.CRUSH_GATE,
-        #     msg_to_receive = IVCMessageType.OOGWAY_ACKNOWLEDGE, timeout=10, parent=self),
-        # move_tasks.move_with_directions([(1, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-
-        # comp_tasks.slalom_task_dead_reckoning(depth_level=1.1, parent=self),
-        # comp_tasks.crush_ivc_receive(msg_to_receive = IVCMessageType.OOGWAY_GATE,
-        #     msg_to_send = IVCMessageType.CRUSH_ACKNOWLEDGE, timeout=90, parent=self),
-        # comp_tasks.return_task_dead_reckoning(depth_level=1.1, parent=self),
-
-        # || START 2025 COMP FLOW OCTAGON
-        comp_tasks.delineate_ivc_log(parent = self),
+        ######## Main competition tasks ########
+        ivc_tasks.delineate_ivc_log(parent=self),
         comp_tasks.initial_submerge(-0.5, z_tolerance=0.15, enable_controls_flag=True, timeout=10, parent=self),
         comp_tasks.coin_flip(enable_same_direction=False, parent=self),
-        comp_tasks.gate_task_dead_reckoning(depth_level=0.7, parent=self), # Move through gate via 2,2; right strafe via 1.5
-        comp_tasks.gate_style_task(depth_level=0.975, parent=self), # Spin
-        comp_tasks.slalom_task_dead_reckoning(depth_level=0.975, parent=self), # Move through slalom via 2,2,2
-        comp_tasks.slalom_to_octagon_dead_reckoning(depth_level=0.975,parent = self), # Move to octagon front via 2,2; left strafe via 0.75
-        comp_tasks.crush_ivc_spam(msg_to_send = IVCMessageType.CRUSH_OCTAGON, parent = self),
+        comp_tasks.gate_task_dead_reckoning(depth_level=0.7, parent=self),  # Move through gate via 2,2; right strafe via 1.5
+        comp_tasks.gate_style_task(depth_level=0.975, parent=self),  # Spin
+        comp_tasks.slalom_task_dead_reckoning(depth_level=0.975, parent=self),  # Move through slalom via 2,2,2
+        comp_tasks.slalom_to_octagon_dead_reckoning(depth_level=0.975, parent=self),  # Move to octagon front via 2,2; left strafe via 0.75
+        ivc_tasks.crush_ivc_spam(msg_to_send=IVCMessageType.CRUSH_OCTAGON, parent=self),
 
-        # || END 2025 COMP FLOW OCTAGON
-
-        # comp_tasks.octagon_task(direction=1, parent = self),
-
-        # comp_tasks.initial_submerge(-0.4, z_tolerance=0.15, enable_controls_flag=True, timeout=10, parent=self),
-        # move_tasks.move_with_directions([(1, 0, 0)], depth_level=-0.4, correct_depth=True, correct_yaw=True, parent=self),
-        # comp_tasks.crush_ivc_send(msg_to_send = IVCMessageType.CRUSH_GATE,
-        #      msg_to_receive = IVCMessageType.OOGWAY_ACKNOWLEDGE, timeout=10, parent=self),
-        # comp_tasks.gate_style_task(depth_level=0.9, parent=self),
-        # move_tasks.yaw_from_local_pose(np.pi/2, parent=self),
-        # move_tasks.move_with_directions([(0.25, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.yaw_from_local_pose(np.pi/2, parent=self),
-        # comp_tasks.orient_to_wall(parent=self),
-        # comp_tasks.orient_to_wall(parent=self),
-        # # move_tasks.yaw_from_local_pose(-np.pi/2, parent=self),
-        # move_tasks.move_with_directions([(0.25, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.yaw_from_local_pose(np.pi/2, parent = self),
-
-        # comp_tasks.octagon_task(direction=1, parent = self),
-        # comp_tasks.gate_style_task(depth_level=0.9, parent=self),
-        # move_tasks.move_with_directions([(0, -2, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(2, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(2, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-
-        # comp_tasks.initial_submerge(-0.3, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # move_tasks.move_with_directions([(0.5, 0, 0)], depth_level=-0.3, correct_depth=True, correct_yaw=True, parent=self),
-
-        # move_tasks.yaw_from_local_pose(-np.pi/4, timeout=10, parent=self),
-        # comp_tasks.crush_ivc_send(msg_to_send = IVCMessageType.CRUSH_GATE,
-        #      msg_to_receive = IVCMessageType.OOGWAY_ACKNOWLEDGE, timeout=90, parent=self),
-        # comp_tasks.gate_to_octagon(timeout = 15, parent = self),
-        # comp_tasks.octagon_task(direction=1, parent = self),
-
-        # SONAR
-        # comp_tasks.initial_submerge(-0.3, z_tolerance=0.15, enable_controls_flag=True, timeout=10, parent=self),
-        # comp_tasks.orient_to_wall(start_angle=-15.0, end_angle=15.0, distance=20.0, parent=self),
-
-        # comp_tasks.initial_submerge(-1.0, z_tolerance=0.15, enable_controls_flag=True, timeout=10, parent=self),
-        # comp_tasks.octagon_task(direction=1, parent = self),
-
-        # comp_tasks.initial_submerge(-0.7, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # comp_tasks.crush_robot_ivc(IVCMessageType.CRUSH_GATE, parent = self),
-        # move_tasks.move_with_directions([(0.5, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.yaw_from_local_pose(np.pi/2, parent = self),
-        # move_tasks.move_with_directions([(0, -2, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(0, 2, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.yaw_from_local_pose(-np.pi/2, parent = self),
-        # move_tasks.move_with_directions([(0, -2, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-
-        # comp_tasks.octagon_task(direction=1, parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.BIN_PINK_FRONT, direction=1, depth_level=1.3, yaw_threshold = math.radians(20), parent=self),
-        # move_tasks.yaw_from_local_pose(np.pi/2, parent = self),
-        # move_tasks.move_with_directions([(0, -2, 0)], depth_level=-0.6, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(0, 2, 0)], depth_level=-0.6, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.yaw_from_local_pose(-np.pi/2, parent = self),
-        # move_tasks.move_with_directions([(2, 0, 0)], depth_level=-0.6, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(0, 1, 0)], depth_level=-0.6, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(-1, 0, 0)], depth_level=-0.6, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(0, -1, 0)], depth_level=-0.6, correct_depth=True, correct_yaw=True, parent=self),
-
-        # comp_tasks.gate_to_octagon(direction=1, move_forward=0, timeout=15, parent=self),
-        # comp_tasks.octagon_task(direction=1, parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.BUOY, direction=1, depth_level=0.2, parent=self),
-
-        # comp_tasks.gate_to_octagon(direction=1, move_forward=0, parent=self),
-        # comp_tasks.octagon_task(direction=1, parent=self),
-
-        # Testing/Garbage Below
-
-        # comp_tasks.initial_submerge(-0.4, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # move_tasks.move_with_directions([(1, 0, 0)], depth_level=-0.4, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(0, 1, 0)], depth_level=-0.4, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(-1, 0, 0)], depth_level=-0.4, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(0, -1, 0)], depth_level=-0.4, correct_depth=True, correct_yaw=True, parent=self),
-
-        # comp_tasks.gate_task_dead_reckoning(),
-        # comp_tasks.initial_submerge(-0.5, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # comp_tasks.crush_robot_ivc(IVCMessageType.CRUSH_GATE, parent = self),
-        # ivc_tasks.test_ivc(IVCMessageType.CRUSH_TEST, parent=self),
-        # buoyancy_tasks.tune_static_power(parent=self),
-        # comp_tasks.initial_submerge(-0.5, z_tolerance=0.15, enable_controls_flag=True, parent=self),
-        # move_tasks.move_to_pose_local(geometry_utils.create_pose(0, 0, 0, 0, 0, -np.pi/2),
-                                      #pose_tolerances=move_tasks.create_twist_tolerance(angular_yaw=0.2),
-                                    #   parent = self),
-        # comp_tasks.octagon_task(direction=1, parent=self),
-        # comp_tasks.coin_flip(parent=self, enable_same_direction=False),
-        # move_tasks.move_with_directions([(1, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.9, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_with_directions([(3, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
-        # move_tasks.move_to_pose_local(geometry_utils.create_pose(0, 0, 0, 0, 0, -np.pi / 2), parent = self),
-        # move_tasks.move_with_directions([(0, 2, 0)], depth_level=-0.9, correct_depth=True, parent=self),
-        # move_tasks.move_with_directions([(0, 2, 0)], depth_level=-0.9, correct_depth=True, parent=self),
-        # move_tasks.move_with_directions([(0.5, 0, 0), (0, 0.5, 0), (-0.5, 0, 0), (0, -0.5, 0)], parent=self),
-        # move_tasks.move_with_directions([(1, 0, 0), (1, 0, 0), (1, 0, 0), (1, 0, 0)], parent=self),
-        # move_tasks.move_with_directions([(1, 0, 0), (-1, 0, 0)], depth_level=-0.5, parent=self),
-        # move_tasks.move_with_directions([(0, 0, -0.5), (0, 0, 0.5)], parent=self),
-        # prequal_tasks.prequal_task(parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.GATE_SAWFISH, direction=1, yaw_threshold=radians(10),
-        #                             latency_threshold=1, depth_level=0.7, parent=self),
+        ######## Unused competition tasks ########
+        ## Gate
         # comp_tasks.gate_task(offset=-0.1, direction=-1, parent=self),
-        # comp_tasks.gate_style_task(depth_level=0.7, parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.BUOY, direction=1, depth_level=0.7, parent=self),
-        # comp_tasks.buoy_task(turn_to_face_buoy=False, depth=0.7, parent=self),
-        # comp_tasks.after_buoy_task(parent=self),
 
-        # sonar_tasks.test_sonar(-60, 60, 5, parent=self),
-
-        # comp_tasks.first_robot_ivc(self, IVCMessageType.CRUSH_GATE)
+        ## Path marker
         # comp_tasks.align_path_marker(direction=-1, parent=self),
-        # comp_tasks.center_path_marker(parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.PATH_MARKER, yaw_threshold=radians(5), direction=-1,
-        #                             depth_level=0.5, parent=self),
         # comp_tasks.path_marker_to_pink_bin(maximum_distance=6, parent=self),
-        # comp_tasks.buoy_to_octagon(direction=1, move_forward=0, parent=self),
+
+        ## Bins/Marker dropper
         # comp_tasks.spiral_bin_search(parent=self),
         # comp_tasks.bin_task(parent=self),
-        # comp_tasks.yaw_to_cv_object('bin_pink_front', direction=-1, yaw_threshold=radians(15),
-        #                             depth_level=1.0, parent=self),
+
+        ## Octagon
         # comp_tasks.octagon_task(direction=1, parent=self),
+        # comp_tasks.orient_to_wall(parent=self),
+        # comp_tasks.gate_to_octagon(direction=1, move_forward=0, timeout=15, parent=self),
+
+        ## Return home
+        # comp_tasks.return_task_dead_reckoning(depth_level=1.1, parent=self),
+
+        ## IVC
+        # ivc_tasks.crush_ivc_send(msg_to_send=IVCMessageType.CRUSH_GATE,
+        #     msg_to_receive=IVCMessageType.OOGWAY_ACKNOWLEDGE, timeout=90, parent=self),
+        # ivc_tasks.crush_ivc_receive(msg_to_receive=IVCMessageType.OOGWAY_GATE,
+        #     msg_to_send=IVCMessageType.CRUSH_ACKNOWLEDGE, timeout=90, parent=self),
+
+        ## Movement/CV tasks
+        # move_tasks.yaw_from_local_pose(np.pi / 2, parent=self),
+        # move_tasks.move_with_directions([(1, 0, 0)], depth_level=-0.7, correct_depth=True, correct_yaw=True, parent=self),
+
+        # comp_tasks.yaw_to_cv_object(CVObjectType.GATE_SAWFISH, direction=1, yaw_threshold=radians(10),
+        #                             latency_threshold=1, depth_level=0.7, parent=self),
+
+        # move_tasks.move_to_pose_local(geometry_utils.create_pose(0, 0, 0, 0, 0, -np.pi/2),
+        #                               pose_tolerances=move_tasks.create_twist_tolerance(angular_yaw=0.2),
+        #                               parent=self),
+
+        ######## Setup/Testing tasks ########
+        # sonar_tasks.test_sonar(-60, 60, 5, parent=self),
+        # ivc_tasks.test_ivc(IVCMessageType.CRUSH_TEST, parent=self),
+        # buoyancy_tasks.tune_static_power(parent=self),
+
+        ######## Prequal tasks ########
+        # prequal_tasks.prequal_task(parent=self),
     ]
 
     for task_to_run in tasks:
--- onboard/src/task_planning/task_planning/robot/oogway.py
+++ onboard/src/task_planning/task_planning/robot/oogway.py
@@ -1,111 +1,70 @@
-# ruff: noqa: ERA001, F401
+# ruff: noqa: ERA001, F401, N806
 from math import radians
-import math
 
-from rclpy.duration import Duration
 from task_planning.interface.cv import CVObjectType
-from task_planning.task import Task, task
-from task_planning.tasks import buoyancy_tasks, comp_tasks, ivc_tasks, sonar_tasks, move_tasks, prequal_tasks
-from task_planning.utils import geometry_utils
 from task_planning.interface.ivc import IVCMessageType
-from task_planning.interface.servos import Servos, MarkerDropperStates, TorpedoStates
-from task_planning.tasks import util_tasks
-from rclpy.clock import Clock
-from rclpy.logging import get_logger
+from task_planning.task import Task, task
+from task_planning.tasks import buoyancy_tasks, comp_tasks, ivc_tasks, move_tasks, prequal_tasks, sonar_tasks
+from task_planning.utils import geometry_utils
 
-logger = get_logger('oogway')
 
 @task
 async def main(self: Task) -> Task[None, None, None]:
     """Run the tasks to be performed by Oogway."""
-
-    ### START OF CONSTANTS ###
+    # Constants
     DIRECTION_OF_TORPEDO_BANNER = 1
     DEPTH = 1
-    START_TIME = 1755374899 # Run secs alias
-    FIRST_TARGET = CVObjectType.TORPEDO_REEF_SHARK_TARGET # CVObjectType.TORPEDO_REEF_SHARK_TARGET or CVObjectType.TORPEDO_SAWFISH_TARGET
-    ### END OF CONSTANTS ###
-
-    # Don't modify
-    END_OF_IVC = START_TIME + (10 * 60)  # Add 10 minutes to START_TIME
-    IVC_TIMEOUT = END_OF_IVC - Clock().now().seconds_nanoseconds()[0]
-    logger.info(f'IVC timeout: {IVC_TIMEOUT} seconds')
+    FIRST_TARGET = CVObjectType.TORPEDO_REEF_SHARK_TARGET  # CVObjectType.TORPEDO_REEF_SHARK_TARGET or CVObjectType.TORPEDO_SAWFISH_TARGET
 
     tasks = [
-        # comp_tasks.initial_submerge(-1.2, parent=self),
-        # test(parent=self),
-        # comp_tasks.initial_submerge(-0.4, parent=self),
+        ######## Main competition tasks ########
+        ivc_tasks.delineate_ivc_log(parent=self),
+        comp_tasks.initial_submerge(-DEPTH, parent=self),
+        comp_tasks.gate_task_dead_reckoning(depth_level=-DEPTH, parent=self),
+        comp_tasks.torpedo_task(first_target=FIRST_TARGET, depth_level=DEPTH, direction=DIRECTION_OF_TORPEDO_BANNER, parent=self),
+        # TODO: task not found???
+        comp_tasks.send_torpedo_ivc(parent=self),
+        comp_tasks.octagon_task(direction=1, parent=self),
+
+        ######## Unused competition tasks ########
+        ## Coin flip
+        # comp_tasks.coin_flip(parent=self),
+
+        ## Gate
+        # comp_tasks.gate_task(offset=-0.1, direction=-1, parent=self),
+        # comp_tasks.gate_style_task(depth_level=0.9, parent=self),
+
+        ## Path marker
+        # comp_tasks.align_path_marker(direction=-1, parent=self),
+        # comp_tasks.path_marker_to_pink_bin(maximum_distance=6, parent=self),
+
+        ## Bins/Marker dropper
+        # comp_tasks.spiral_bin_search(parent=self),
+        # comp_tasks.bin_task(parent=self),
+
+        ## Octagon
+        # comp_tasks.octagon_task(direction=1, parent=self),
+
+        ## IVC
+        # TODO: task not found???
         # comp_tasks.oogway_ivc_start(IVCMessageType.OOGWAY_ACKNOWLEDGE, parent=self),
+
+        ## Movement/CV tasks
+        # move_tasks.move_with_directions([(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0)], parent=self),
         # move_tasks.move_to_pose_local(
         #     geometry_utils.create_pose(0, 0, 0, 0, 0, 1.6),
         #     parent=self,
         # ),
+        # comp_tasks.yaw_to_cv_object(CVObjectType.GATE_SAWFISH, direction=1, yaw_threshold=radians(10),
+        #                             latency_threshold=1, depth_level=0.7, parent=self),
 
-        # comp_tasks.yaw_to_cv_object(CVObjectType.TORPEDO_BANNER, direction=1, yaw_threshold=math.radians(10), parent=self)
-
-        # Home pool test 8/13
-        # comp_tasks.initial_submerge(-DEPTH, parent=self),
-        # comp_tasks.oogway_ivc_start(IVCMessageType.OOGWAY_ACKNOWLEDGE, parent=self),
-        # comp_tasks.gate_task_dead_reckoning(depth_level=-DEPTH, parent=self),
-        # comp_tasks.torpedo_task(depth_level=DEPTH, direction=DIRECTION_OF_TORPEDO_BANNER, parent=self),
-
-        # IVC 8/13
-        # comp_tasks.oogway_ivc_start(IVCMessageType.OOGWAY_ACKNOWLEDGE, parent=self), # Crush thru gate, Oogway acknowledges
-        # comp_tasks.initial_submerge(-0.8, parent=self), # Oogway gate
-        # Oogway does stuff
-        # Oogway sends Crush done
-        # Crush returns to gate
-
-
-        ##### COMP
-        comp_tasks.delineate_ivc_log(parent=self),
-        comp_tasks.initial_submerge(-DEPTH, parent=self),
-        comp_tasks.gate_task_dead_reckoning(depth_level=-DEPTH, parent=self),
-        comp_tasks.torpedo_task(first_target=FIRST_TARGET, depth_level=DEPTH, direction=DIRECTION_OF_TORPEDO_BANNER, parent=self),
-        comp_tasks.send_torpedo_ivc(parent=self),
-        comp_tasks.octagon_task(direction=1, parent=self),
-        ##### END COMP
-
-        # comp_tasks.initial_submerge(-1.2, parent=self),
-
-
-        # comp_tasks.yaw_to_cv_object(CVObjectType.TORPEDO_BANNER, direction=1, yaw_threshold=math.radians(10),
-        #     depth_level=0.7, parent=self),
-
-
-        # comp_tasks.coin_flip(parent=self),
-        # comp_tasks.gate_task(steps=3, parent=self),
-        # comp_tasks.gate_style_task(parent=self),
-        # comp_tasks.gate_task(offset=-0.1, direction=-1, parent=self),
-        # comp_tasks.gate_style_task(depth_level=0.9, parent=self),
+        ######## Setup/Testing tasks ########
         # sonar_tasks.test_sonar(-45, 45, 5, parent=self),
         # ivc_tasks.test_ivc(IVCMessageType.OOGWAY_TEST, parent=self),
         # buoyancy_tasks.buoyancy_task(-0.5, parent=self),  # Submerge and stabilize buoyancy
-        # move_tasks.move_with_directions([(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0)], parent=self),
-        # move_tasks.move_with_directions([(2, 0, 0), (0, 2, 0), (-2, 0, 0), (0, -2, 0)], parent=self),
 
+        ######## Prequal tasks ########
         # prequal_tasks.prequal_task(parent=self),
-
-        # comp_tasks.coin_flip(parent=self),
-        # comp_tasks.second_robot_ivc(self, IVCMessageType.OOGWAY_ACKNOWLEDGE)
-
-        # comp_tasks.yaw_to_cv_object(CVObjectType.GATE_SAWFISH, direction=1, yaw_threshold=radians(10),
-        #                             latency_threshold=1, depth_level=0.7, parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.BUOY, direction=1, depth_level=0.7, parent=self),
-        # comp_tasks.buoy_task(turn_to_face_buoy=False, depth=0.7, parent=self),
-        # comp_tasks.after_buoy_task(parent=self),
-
-        # comp_tasks.align_path_marker(direction=-1, parent=self),
-        # comp_tasks.center_path_marker(parent=self),
-        # comp_tasks.yaw_to_cv_object(CVObjectType.PATH_MARKER, yaw_threshold=radians(5), direction=-1,
-        #                             depth_level=0.5, parent=self),
-        # comp_tasks.path_marker_to_pink_bin(maximum_distance=6, parent=self),
-        # comp_tasks.buoy_to_octagon(direction=1, move_forward=0, parent=self),
-        # comp_tasks.spiral_bin_search(parent=self),
-        # comp_tasks.bin_task(parent=self),
-        # comp_tasks.yaw_to_cv_object('bin_pink_front', direction=-1, yaw_threshold=radians(15),
-        #                             depth_level=1.0, parent=self),
-        # comp_tasks.octagon_task(direction=1, parent=self),
     ]
 
     for task_to_run in tasks:
