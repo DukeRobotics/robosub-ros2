fb7507bb104dbff58e61f262ac45219ec08bf532
AnthonZh <anthonyzh3349@gmail.com>
2025-10-24 00:10:16 +0000

    Sonar data from pool test on 10/19 added to /onboard/src/sonar/sweep_data. Docker-build.sh changed because of issues with personal computer.

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,7 @@
 # Check if the first argument is "skip-wsl" and if the script is running in WSL
 # This is used by the Dev Container to avoid starting the container via this script when running on Windows
 if [[ "$1" == "skip-wsl" && -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+#if [[ "$1" == "skip-wsl" ]]; then
     echo "'skip-wsl' is specified and the script is running in WSL. Exiting..."
     exit 0
 fi
