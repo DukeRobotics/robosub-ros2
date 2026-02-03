74ec9f00022ba0ac53b54ef11bcb2bb6a41f0716
AnthonZh <anthonyzh3349@gmail.com>
2026-02-03 21:19:21 +0000

    Removed foxglove messages

--- onboard/src/cv/cv/depthai_spatial_detection.py
+++ onboard/src/cv/cv/depthai_spatial_detection.py
@@ -458,7 +458,7 @@ class DepthAISpatialDetector(Node):
         # distortion coefficients
         dist = calibData.getDistortionModel(dai.CameraBoardSocket.RGB)
         distortion_coefficients = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.RIGHT))
-        print(f'Distortion Coefficients (OpenCV style): {distortion_coefficients}')
+        self.get_logger().info(f'Distortion Coefficients (OpenCV style): {distortion_coefficients}')
 
         # Intrinsics: [[2297.609375, 0.0, 1901.7725830078125], [0.0, 2297.609375, 1104.69873046875], [0.0, 0.0, 1.0]] Distortion: CameraModel.Perspective
 
