4cc8b78663748d8faf81488ab8718d5e3521d480
Oogway <oogway@duke-robotics.com>
2026-02-22 19:02:57 +0000

    Revert "Added cases for rotate_to_normal"
    This reverts commit bec9f5b2c96e437a264b94c0cb9cee755b9c0ffc.

--- onboard/src/task_planning/task_planning/tasks/sonar_tasks.py
+++ onboard/src/task_planning/task_planning/tasks/sonar_tasks.py
@@ -162,10 +162,6 @@ async def get_normal_angle(start_angle: float, end_angle: float, scan_distance: 
     if response.normal_angle is None:
         logger.error('[Sonar] normal_angle was None — cannot rotate')
         return np.nan
-
-    if response.normal_angle < -np.pi/2.:
-        return response.normal_angle + np.pi
-
     return response.normal_angle
 
 async def get_normal_and_wall_angle(
