111014afd3263efff726ba53830cfb3099100c7a
Oogway <oogway@duke-robotics.com>
2026-07-11 05:07:14 -0400

    changes cv

--- onboard/src/cv/setup.py
+++ onboard/src/cv/setup.py
@@ -36,6 +36,7 @@ setup(
             'path_marker_detector = cv.path_marker_detector:main',
             'pink_bins_detector = cv.pink_bins_detector:main',
             'torpedo_target_detector = cv.torpedo_target_detector:main',
+            'torpedo_target_detector_2026 = cv.torpedo_target_detector_2026:main',
             'usb_camera = cv.usb_camera:main',
             'hsv_filter = cv.hsv_filter:main',
             'hsv_red_bin = cv.hsv_red_bin:main',
