001a26ee1ac2f837ba75e545352bbd838fcfb8ee
Mathew Chu <mathewchukc@gmail.com>
2026-07-12 07:43:47 -0700

    task planning oog

--- onboard/src/task_planning/task_planning/robot/oogway.py
+++ onboard/src/task_planning/task_planning/robot/oogway.py
@@ -30,7 +30,7 @@ async def main(self: Task) -> Task[None, None, None]:
         ######## Main competition tasks ########
         #ivc_tasks.delineate_ivc_log(parent=self),
         comp_tasks.initial_submerge(DEPTH, parent=self),
-        sonar_tasks.
+        comp_tasks.torpedo_task_2026(first_target=FIRST_TARGET, second_target=SECOND_TARGET, depth_level=DEPTH, direction=DIRECTION_OF_TORPEDO_BANNER, parent=self),
         # move_tasks.move_with_directions([(2.5, 0, 0), (0, 2.5, 0), (-2.5, 0, 0), (0, -2.5, 0)], parent=self),
         # move_tasks.move_with_directions([(1, 0, 0), (0, 0.5, 0), (-1, 0, 0), (0, -0.5, 0)], parent=self),
         # comp_tasks.gate_task_dead_reckoning(depth_level=-DEPTH, parent=self),
