9c981741f7496ff60bb330002414e09818ddf851
Oogway <oogway@duke-robotics.com>
2026-07-13 11:33:45 -0400

    BIG SAAG nighttime testing config

--- onboard/src/cv/cv/config.py
+++ onboard/src/cv/cv/config.py
@@ -42,6 +42,12 @@ class Torpedo:
     HIGH_BOT = np.array([360, 50, 45])
     HIGH_TOP = np.array([360, 50, 45])
 
+    # Nighttime values
+    # LOW_BOT = np.array([0, 30, 55])
+    # LOW_TOP = np.array([5, 40, 95])
+    # HIGH_BOT = np.array([345, 30, 55])
+    # HIGH_TOP = np.array([360, 40, 95])
+
     TORPEDO_BANNER_X_SCALE = 1.2
     TORPEDO_BANNER_Y_SCALE = 0.5
 
